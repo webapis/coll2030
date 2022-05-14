961ebddf56837dfe57be3b10b27d805323eae70b
webapis <60083568+webapis@users.noreply.github.com>
2022-05-14 10:27:51 +0500

    Update placeholders.js

--- src/placeholders.js
+++ src/placeholders.js
@@ -1,3 +1,4 @@
 export default {
-    defacto: { placeholder: 'https://dfcdn.defacto.com.tr/AssetsV2/dist/img/placeholders/placeholder.svg', imageHost: '	https://dfcdn.defacto.com.tr/', detailHost: 'https://www.defacto.com.tr/' }
+    defacto: { placeholder: 'https://dfcdn.defacto.com.tr/AssetsV2/dist/img/placeholders/placeholder.svg', imageHost: 'https://dfcdn.defacto.com.tr/', detailHost: 'https://www.defacto.com.tr/' },
+    koton: { placeholder: 'http://img-kotonw.mncdn.com/_ui/shared/images/koton-loading-gif2.gif', imageHost: 'https://ktnimg.mncdn.com/mnresize/', detailHost: 'https://www.koton.com/tr/' }
 }
