75863388e6e6df5be78972b8e882aedaa6ed0e48
webapis <60083568+webapis@users.noreply.github.com>
2022-05-16 14:52:02 +0500

    v

--- main.js
+++ main.js
@@ -235,19 +235,68 @@ Apify.main(async () => {
 
 
 function order(items) {
-    const groupByCategory = items.sort((a, b) => (a.subcategory > b.subcategory) ? 1 : -1).reduce((group, product) => {
+    const groupByMarka = items.sort((a, b) => (a.subcategory > b.subcategory) ? 1 : -1).reduce((group, product) => {
         const { marka } = product;
         group[marka] = group[marka] ?? [];
         group[marka].push(product);
         return group;
     }, {});
-    const orderedByMarka = []
-    for (let g in groupByCategory) {
-        const current = groupByCategory[g]
-        orderedByMarka.push(...current.map((c, i) => { return { ...c, itemOrder: i } }))
+
+    const groupbysub = {}
+
+    for (let c in groupByMarka) {
+        const current = groupByMarka[c]
+
+        groupbysub[c] = {
+            total: current.length, subcategories: current.reduce((group, product) => {
+                const { subcategory } = product;
+
+                group[subcategory] = group[subcategory] ?? [];
+                group[subcategory].push(product);
+                return group;
+            }, {})
+        }
+
+
+
     }
 
-    return orderedByMarka.sort((a, b) => (a.itemOrder > b.itemOrder) ? 1 : -1)
+    const addItemOrder = {}
+    for (let c in groupbysub) {
+        const { subcategories } = groupbysub[c]
+        addItemOrder[c] = {}
+        for (let s in subcategories) {
+
+            addItemOrder[c][s] = []
+
+            const current = subcategories[s].map((sk, i) => { return { ...sk, itemOrder: i } })
+
+            addItemOrder[c][s] = current
+
+
+        }
+
+
+    }
+    let flatten = []
+    for (let c in groupbysub) {
+        const { subcategories } = groupbysub[c]
+
+        for (let s in subcategories) {
+
+
+
+            const current = subcategories[s]
+
+            flatten.push(...current)
+
+
+        }
+
+
+    }
+
+    return flatten.sort((a, b) => (a.itemOrder > b.itemOrder) ? 1 : -1)
 }
 /*
 
--- spine.js
+++ spine.js
@@ -1,7 +1,71 @@
 
 const data = require('./api/_files/kadin/data.json')
-const {navTree}=require('./navTree')
-debugger;
-navTree(data)
+function order(items) {
+    const groupByMarka = items.sort((a, b) => (a.subcategory > b.subcategory) ? 1 : -1).reduce((group, product) => {
+        const { marka } = product;
+        group[marka] = group[marka] ?? [];
+        group[marka].push(product);
+        return group;
+    }, {});
 
-debugger;
+    const groupbysub = {}
+
+    for (let c in groupByMarka) {
+        const current = groupByMarka[c]
+
+        groupbysub[c] = {
+            total: current.length, subcategories: current.reduce((group, product) => {
+                const { subcategory } = product;
+
+                group[subcategory] = group[subcategory] ?? [];
+                group[subcategory].push(product);
+                return group;
+            }, {})
+        }
+
+
+
+    }
+
+    const addItemOrder = {}
+    for (let c in groupbysub) {
+        const { subcategories } = groupbysub[c]
+        addItemOrder[c] = {}
+        for (let s in subcategories) {
+
+            addItemOrder[c][s] = []
+
+            const current = subcategories[s].map((sk, i) => { return { ...sk, itemOrder: i } })
+
+            addItemOrder[c][s] = current
+
+
+        }
+
+
+    }
+    let flatten = []
+    for (let c in groupbysub) {
+        const { subcategories } = groupbysub[c]
+
+        for (let s in subcategories) {
+
+
+
+            const current = subcategories[s]
+
+            flatten.push(...current)
+
+
+        }
+
+
+    }
+
+
+
+
+    return flatten.sort((a, b) => (a.itemOrder > b.itemOrder) ? 1 : -1)
+}
+
+order(data)
