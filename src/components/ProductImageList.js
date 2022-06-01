f55b4578d291d35d686d19294d9f56e57f762b4d
webapis <60083568+webapis@users.noreply.github.com>
2022-06-01 14:00:50 +0500

    Update ProductImageList.js

--- src/components/ProductImageList.js
+++ src/components/ProductImageList.js
@@ -1,46 +1,46 @@
 
 import React, { useEffect, useState } from 'react';
-import { useSelector,useDispatch } from 'react-redux';
-
+import { useSelector, useDispatch } from 'react-redux';
+import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid'
 //import IntersectionObserver from "../intersectObserver";
 import ImageComponent from './imageComponent';
 import CircularProgress from '@mui/material/CircularProgress';
-import {actions} from '../store/breadcrumbSlice'
+import { actions } from '../store/breadcrumbSlice'
 import Button from '@mui/material/Button';
 
 
-const getWidth = () => window.innerWidth 
-  || document.documentElement.clientWidth 
+const getWidth = () => window.innerWidth
+  || document.documentElement.clientWidth
   || document.body.clientWidth;
 export default function ProductImageList(props) {
   const [state, setData] = useState([]);
-  const {selectedMarka,selectedSubcategory,totalFetchedProducts,subCatTotal,fetching,products} =useSelector(state=>state.breadcrumb)
+  const { selectedMarka, selectedSubcategory, totalFetchedProducts, subCatTotal, fetching, products } = useSelector(state => state.breadcrumb)
 
-  const dispatch =useDispatch()
+  const dispatch = useDispatch()
   debugger;
   let [width, setWidth] = useState(getWidth());
 
-useEffect(()=>{
-  const width =getWidth()
-  
+  useEffect(() => {
+    const width = getWidth()
 
 
-  const resizeListener = () => {
-    // change width from the state object
-  
-    setWidth(getWidth())
+
+    const resizeListener = () => {
+      // change width from the state object
+
+      setWidth(getWidth())
 
 
-  };
-  window.addEventListener('resize', resizeListener);
+    };
+    window.addEventListener('resize', resizeListener);
 
-  // clean up function
-  return () => {
-    // remove resize listener
-    window.removeEventListener('resize', resizeListener);
-  }
-},[])
+    // clean up function
+    return () => {
+      // remove resize listener
+      window.removeEventListener('resize', resizeListener);
+    }
+  }, [])
 
   useEffect(() => {
     localStorage.setItem('page', 1)
@@ -56,7 +56,7 @@ useEffect(()=>{
     //   const onIntersect = (bool, entry) => {
     //     entry.target.src = entry.target.dataset.src;
 
-       
+
     //   };
 
     //   const intersectObserver = new IntersectionObserver({
@@ -72,15 +72,15 @@ useEffect(()=>{
 
   async function fetchData(page) {
 
-debugger;
+    debugger;
     const url = `/api/kadin/data?page=${page}&subcategory=${selectedSubcategory}&marka=${selectedMarka}`
     const response = await fetch(url, { cache: 'default' })
 
     const { data } = await response.json()
 
-    dispatch(actions.setFetchedProductsTotal({products:data}))
+    dispatch(actions.setFetchedProductsTotal({ products: data }))
     dispatch(actions.setFetchState(false))
-   // setData(prevState => [...prevState, ...data])
+    // setData(prevState => [...prevState, ...data])
 
 
   }
@@ -97,9 +97,9 @@ debugger;
 
     <Grid container justifyContent="center" spacing={0} paddingTop={5}
     >
-      {products.length>0 && products.map((item, i) => {
+      {products.length > 0 && products.map((item, i) => {
 
-        return <Grid item key={i} xs={6} sm={4} md={3} lg={2}  sx={{ display: 'flex', justifyContent: 'center' }}>
+        return <Grid item key={i} xs={6} sm={4} md={3} lg={2} sx={{ display: 'flex', justifyContent: 'center' }}>
 
           <ImageComponent plcHolder={item.plcHolder} imageUrl={item.imageUrl} title={item.title} marka={item.marka} link={item.link} timestamp={item.timestamp} price={item.priceNew} />
 
@@ -107,7 +107,9 @@ debugger;
       })}
 
       <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'center', marginBottom: 5 }}>
-        {products.length > 0 ?<div>{fetching?<CircularProgress />:<Button disabled={(totalFetchedProducts) >=subCatTotal} variant='outlined' onClick={fetchNextPage}>{(totalFetchedProducts)}/{subCatTotal}</Button>}</div>  : <CircularProgress />}
+        {products.length > 0 ? <div>{fetching ? <Box sx={{ display: 'flex' }}>
+          <CircularProgress />
+        </Box> : <Button disabled={(totalFetchedProducts) >= subCatTotal} variant='outlined' onClick={fetchNextPage}>{(totalFetchedProducts)}/{subCatTotal}</Button>}</div> : <CircularProgress />}
       </Grid>
     </Grid>
 
