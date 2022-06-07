94fc186ae9af93dd9f3a2b5a5d883e278e97955c
webapis <60083568+webapis@users.noreply.github.com>
2022-06-07 15:27:34 +0500

    vv

--- src/components/FilterResult.js
+++ src/components/FilterResult.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import ProductImageList from './ProductImageList'
 
-import MarkaMenu from './markaMenu';
+
 import { actions } from '../store/breadcrumbSlice'
 import { useDispatch, useSelector } from 'react-redux';
 import Grid from '@mui/material/Grid';
@@ -10,13 +10,11 @@ import { Container } from '@mui/material';
 import Button from '@mui/material/Button';
 import Chip from '@mui/material/Chip';
 export default function FilterResult(props) {
-  const dispatch = useDispatch()
+
   const selectedTab = useSelector(state => state.breadcrumb.selectedTab)
 
 
-  function handleChange(event, tabId) {
-    dispatch(actions.selectTab(tabId))
-  }
+ 
 
   return (
     <Box>
@@ -27,7 +25,7 @@ export default function FilterResult(props) {
 
       </TabPanel>
       <TabPanel value={selectedTab} index={1} sx={{ display: 'flex', justifyContent: 'center' }}>
-        <MarkaMenu sx={{ height: "100%" }} handleTabChange={handleChange} />
+        
       </TabPanel>
       <TabPanel value={selectedTab} index={2}>
         <ProductImageList />
