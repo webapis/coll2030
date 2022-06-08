e3711293b5b786e14cdea77d50bc0206a58d593a
webapis <60083568+webapis@users.noreply.github.com>
2022-06-08 13:30:14 +0500

    Update data.js

--- api/kadin/data.js
+++ api/kadin/data.js
@@ -4,10 +4,11 @@ var TAFFY = require('taffy');
 
 // Create a new database a single object (first record)
 const data = require('../_files/kadin/data.json')
-var products = TAFFY(data);
+
 module.exports = (req, res) => {
     const { subcatregex, categoryregex, page, marka, search } = req.query
     const start = parseInt(page)
+    var products = TAFFY(data);
     const filterBySub = subcatregex === '' ? {} : { title: { regex: new RegExp(subcatregex, 'i') } }
     // for (let f in filterBySub) {
     //     const current = filterBySub[f]
@@ -41,6 +42,7 @@ module.exports = (req, res) => {
   console.log('categoryregex',filterByCat)
   console.log('search',filterBySearch)
   console.log('marka',marka)
+  console.log('page',page)
     debugger;
     res.status(200).json({ data:d })
 }
