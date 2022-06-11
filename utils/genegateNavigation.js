31bdf0650db4ccbf52e7c683effd928f0217ab68
webapis <60083568+webapis@users.noreply.github.com>
2022-06-11 13:17:18 +0500

    v

--- utils/genegateNavigation.js
+++ utils/genegateNavigation.js
@@ -23,66 +23,83 @@ async function generateNavigation() {
         categoryItems.push({ subcategory, category, regex })
     }
 
-    const files = fs.readdirSync('api/_files/kadin')
+
     debugger;
-    for (let file of files) {
+    const data = fs.readFileSync(`api/_files/kadin/data.json`, { encoding: 'utf8' })
+    debugger;
+    const dataCollected = JSON.parse(data)
+    const mapCategory = dataCollected.map((m) => {
+        const match = categoryItems.find(c => {
+            const regex = new RegExp(c.regex, 'i')
+            const result = regex.test(m.title.toLowerCase())
+
+            return result === true
+        })
         debugger;
-        const data = fs.readFileSync(`api/_files/kadin/${file}`, { encoding: 'utf8' })
+        let categoryExistsintitle = match && new RegExp(match.category, 'i').test(m.title)
+
+        let category = match ? categoryExistsintitle ? '' : "_ "+match.category : "belirsiz"
+        console.log('match', category)
+        return {
+            ...m, title: m.title + category
+        }
+    })
+
+    for (let p of mapCategory) {
+        const { title: productTitle, marka } = p
+        const productSubCategories = categoryItems.filter(c => {
+            const regex = new RegExp(c.regex, 'i')
+            const result = regex.test(productTitle.toLowerCase())
+            return result
+        })
         debugger;
-        const dataCollected = JSON.parse(data)
 
-        for (let p of dataCollected) {
-            const { title: productTitle, marka } = p
-            const productSubCategories = categoryItems.filter(c => {
-                const regex = new RegExp(c.regex, 'i')
-                const result = regex.test(productTitle.toLowerCase())
-                return result
-            })
-            debugger;
+        if (productSubCategories.length > 0) {
+            for (let d of productSubCategories) {
+                counter = counter + 1
+                console.log('counter', counter)
+                const { category, subcategory, regex } = d
+                debugger;
+                await categoryNavCollection.updateOne({}, { $inc: { [`nav.totalByCategory`]: 1 } }, { upsert: true })
+                await categoryNavCollection.updateOne({}, { $inc: { [`nav.categories.${category}.totalBySubcategory`]: 1 } }, { upsert: true })
+                await categoryNavCollection.updateOne({}, { $set: { [`nav.categories.${category}.subcategories.${subcategory}.regex`]: regex } }, { upsert: true })
+                await categoryNavCollection.updateOne({}, { $inc: { [`nav.categories.${category}.subcategories.${subcategory}.count`]: 1 } }, { upsert: true })
 
-            if (productSubCategories.length > 0) {
-                for (let d of productSubCategories) {
-                    counter = counter + 1
-                    console.log('counter', counter)
-                    const { category, subcategory, regex } = d
-                    debugger;
-                    await categoryNavCollection.updateOne({}, { $inc: { [`nav.totalByCategory`]: 1 } }, { upsert: true })
-                    await categoryNavCollection.updateOne({}, { $inc: { [`nav.categories.${category}.totalBySubcategory`]: 1 } }, { upsert: true })
-                    await categoryNavCollection.updateOne({}, { $set: { [`nav.categories.${category}.subcategories.${subcategory}.regex`]: regex } }, { upsert: true })
-                    await categoryNavCollection.updateOne({}, { $inc: { [`nav.categories.${category}.subcategories.${subcategory}.count`]: 1 } }, { upsert: true })
-
-
-                    await markaNavCollection.updateOne({}, { $inc: { [`nav.totalByMarka`]: 1 } }, { upsert: true })
-                    await markaNavCollection.updateOne({}, { $inc: { [`nav.markas.${marka}.totalByCatory`]: 1 } }, { upsert: true })
-                    await markaNavCollection.updateOne({}, { $inc: { [`nav.markas.${marka}.categories.${category}.totalBySubcategory`]: 1 } }, { upsert: true })
-                    await markaNavCollection.updateOne({}, { $set: { [`nav.markas.${marka}.categories.${category}.subcategories.${subcategory}.regex`]: regex } }, { upsert: true })
-                    await markaNavCollection.updateOne({}, { $inc: { [`nav.markas.${marka}.categories.${category}.subcategories.${subcategory}.count`]: 1 } }, { upsert: true })
-
-                }
-
-            } else {
-                const findcategory = categoryItems.find(c => {
-                    const regex = new RegExp(c.category, 'i')
-                    const result = regex.test(productTitle.toLowerCase())
-
-                    return result
-                })
-
-                if (findcategory) {
-
-                    //  await productsNavDataset.pushData({ marka, category: findcategory.category, subcategory: 'diğer', regex: 'diğer' })
-                } else {
-
-                    //  await productsNavDataset.pushData({ marka, category: 'belirsiz', subcategory: 'belirsiz', regex: 'belirsiz' })
-                }
+                await markaNavCollection.updateOne({}, { $inc: { [`nav.totalByMarka`]: 1 } }, { upsert: true })
+                await markaNavCollection.updateOne({}, { $inc: { [`nav.markas.${marka}.totalByCatory`]: 1 } }, { upsert: true })
+                await markaNavCollection.updateOne({}, { $inc: { [`nav.markas.${marka}.categories.${category}.totalBySubcategory`]: 1 } }, { upsert: true })
+                await markaNavCollection.updateOne({}, { $set: { [`nav.markas.${marka}.categories.${category}.subcategories.${subcategory}.regex`]: regex } }, { upsert: true })
+                await markaNavCollection.updateOne({}, { $inc: { [`nav.markas.${marka}.categories.${category}.subcategories.${subcategory}.count`]: 1 } }, { upsert: true })
 
             }
 
-            await extractNavData({ collection: categoryNavCollection, exportPath: './src/components/categoryMenu/category-nav.json' })
-            await extractNavData({ collection: markaNavCollection, exportPath: './src/components/MarkaMenu/marka-nav.json' })
-            debugger;
+        } else {
+            const findcategory = categoryItems.find(c => {
+                const regex = new RegExp(c.category, 'i')
+                const result = regex.test(productTitle.toLowerCase())
+
+                return result
+            })
+
+            if (findcategory) {
+
+                //  await productsNavDataset.pushData({ marka, category: findcategory.category, subcategory: 'diğer', regex: 'diğer' })
+            } else {
+
+                //  await productsNavDataset.pushData({ marka, category: 'belirsiz', subcategory: 'belirsiz', regex: 'belirsiz' })
+            }
+
         }
+
+        if (fs.existsSync('api/_files/kadin/data.json')) {
+            fs.unlinkSync('api/_files/kadin/data.json')
+        }
+        fs.appendFileSync('api/_files/kadin/data.json', JSON.stringify(mapCategory))
+        await extractNavData({ collection: categoryNavCollection, exportPath: './src/components/categoryMenu/category-nav.json' })
+        await extractNavData({ collection: markaNavCollection, exportPath: './src/components/MarkaMenu/marka-nav.json' })
+        debugger;
     }
+
 
 }
 
