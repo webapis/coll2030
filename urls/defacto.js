7f5edd5cd0b17865843d0d673a253caf0c769ece
webapis <60083568+webapis@users.noreply.github.com>
2022-06-24 08:29:23 +0500

    v

--- urls/defacto.js
+++ urls/defacto.js
@@ -1,5 +1,6 @@
 const urls = [
   
-    { url: 'https://www.defacto.com.tr/kadin-elbise', subcategory: 'elbise', category: 'üst-giyim' }]
+    { url: 'https://www.defacto.com.tr/kadin-elbise', subcategory: 'elbise', category: 'üst-giyim' }
+    ,{url:'https://www.defacto.com.tr/kadin-pantolon',subcategory: 'pantolon', category: 'alt-giyim'} ]
 
 module.exports={urls}
