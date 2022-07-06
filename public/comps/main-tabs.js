98c8dfef44db49b37eb0f1f77d216b181df28e51
webapis <60083568+webapis@users.noreply.github.com>
2022-07-06 12:51:34 +0500

    b

--- public/comps/main-tabs.js
+++ public/comps/main-tabs.js
@@ -4,10 +4,10 @@ var element =document.getElementById('#bread-crumb-container')
 
 document.getElementById('navbar').insertAdjacentHTML('beforeend', '<div class="container"><ul class="nav nav-tabs justify-content-center m-2">' +
   '<li class="nav-item">' +
-  '<a class="nav-link mtb" aria-current="page" href="/markalar.html" id="marka-tab">Markalar</a>' +
+  '<a class="nav-link mtb" aria-current="page" href="/markalar.html" id="marka-tab">Marka</a>' +
   '</li>' +
   '<li class="nav-item">' +
-  '<a class="nav-link mtb" href="/urunler.html"  id="urun-tab">Ürünler</a>' +
+  '<a class="nav-link mtb" href="/urunler.html"  id="urun-tab">Ürün</a>' +
   '</li>' +
   '<li class="nav-item">' +
   '<a class="nav-link mtb" href="/urunler.html"  id="filter-tab"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-funnel" viewBox="0 0 16 16"><path d="M1.5 1.5A.5.5 0 0 1 2 1h12a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.128.334L10 8.692V13.5a.5.5 0 0 1-.342.474l-3 1A.5.5 0 0 1 6 14.5V8.692L1.628 3.834A.5.5 0 0 1 1.5 3.5v-2zm1 .5v1.308l4.372 4.858A.5.5 0 0 1 7 8.5v5.306l2-.666V8.5a.5.5 0 0 1 .128-.334L13.5 3.308V2h-11z"/></svg></a>' +
