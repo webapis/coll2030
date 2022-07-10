30781e84f36fee027efed575065c57030eef793d
webapis <60083568+webapis@users.noreply.github.com>
2022-07-10 13:36:55 +0500

    v

--- public/adapter.js
+++ public/adapter.js
@@ -5,6 +5,16 @@ if (!Object.entries)
         resArray = new Array(i); // preallocate the Array
     while (i--)
       resArray[i] = [ownProps[i], obj[ownProps[i]]];
-
     return resArray;
   };
+
+  if (!Object.values)
+  {
+    Object.values =function (o)
+    {
+        
+     return Object.keys(o).map(function(k) {return o[k]})
+    }
+
+  }
+  
