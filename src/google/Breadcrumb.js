b474d709bd9bbb927bee206d736d7b142bd0a38f
webapis <60083568+webapis@users.noreply.github.com>
2022-07-18 17:52:37 +0500

    d

--- src/google/Breadcrumb.js
+++ src/google/Breadcrumb.js
@@ -1,20 +1,37 @@
 import React from 'react';
 
-
+import Breadcrumbs from '@mui/material/Breadcrumbs';
 import Container from '@mui/material/Container';
 import Link from '@mui/material/Link';
 import { useSelector, useDispatch } from 'react-redux';
-
+import { actions } from '../store/breadcrumbSlice'
+import { Typography } from '@mui/material';
+import HomeIcon from '@mui/icons-material/Home';
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
 import SearchBox from './SearchBox';
+import { Button } from '@mui/material';
 export default function BreadcrumbsContainer(props) {
+  const { selectedMarka, selectedSubcategory } = useSelector(state => state.maintab)
+  const dispatch = useDispatch()
+  function handleCategoryClick() {
+
+
+  }
+
+  function handleTopBreadClick() {
+
+  }
 
   return (
     <Container sx={{display:'flex', justifyContent:'space-between'}}>
-      <Link href="#" underline="none">
+      <Link href="#">
         MODABURADA
       </Link>
       <div>
-      <SearchBox/>
+      <Button variant="outlined">
+        <SearchIcon />
+      </Button>
       </div>
   
 
