46ca5c5a2016bec731c0c2567612738a3d310289
webapis <60083568+webapis@users.noreply.github.com>
2022-08-05 16:17:52 +0500

    b

--- src/drawer/imageComponent/index.js
+++ src/drawer/imageComponent/index.js
@@ -2,34 +2,19 @@
 import React, { useEffect, useRef } from 'react';
 import placeholders from './placeholders'
 import Typography from '@mui/material/Typography';
-import { useSelector } from 'react-redux';
-
+import { AppContext } from '../../App';
 import { ImageListItem } from '@mui/material';
 import './hl.css'
 
 
 export default function ImageComponent(props) {
-  const { selectedKeywords, parentKeyword } = useSelector(state => state.accordion)
 
-  const { selectedSubcategory } = props
-
-
-  const productTitle =props.title.split(' ').map(t=>t.trim()).map((m,i)=>{
-    
-    const selectedKeywordMatch =selectedKeywords.find(f=>f.keyword.includes(m.toLowerCase()))|| selectedKeywords.map(t=>{ return{...t, keyword: t.keyword.trim()}}).find(s=>m.toLowerCase().includes(s))
-    const selectedSubcategoryMatch =selectedSubcategory.includes(m.toLowerCase()) ||m.toLowerCase().includes(selectedSubcategory)
-
-    
- return <span key={i} style={{fontWeight: (selectedKeywordMatch||selectedSubcategoryMatch)? 800:300 }}>{m}{` `}
-  
-  
-  </span>}) //refactoredTitle  //.substring(props.title.indexOf(" "),props.title.lastIndexOf(" "))
 
   const imageEl = useRef(null);
 
   // const cloudinary = 'https://res.cloudinary.com/codergihub/image/fetch/w_300/'
 
-  const imagePlaceholder = !props.icon ? placeholders[props.marka].placeholder :`/dressicons/${parentKeyword}.jpg`
+  const imagePlaceholder =  placeholders[props.marka].placeholder 
 
   const logo = placeholders[props.marka].logo
 
@@ -43,28 +28,28 @@ export default function ImageComponent(props) {
   var days = Math.floor(minutesdiff / (1000 * 60 * 60 * 24));
   var month = Math.round(minutesdiff / (2e3 * 3600 * 365.25));
   useEffect(() => {
-   
 
-      if (window.IntersectionObserver) {
 
-        let observer = new IntersectionObserver((entries, observer) => {
-          entries.forEach(entry => {
-            if (entry.isIntersecting) {
-  
-              entry.target.src = entry.target.dataset.src;
-              observer.unobserve(entry.target);
-            }
-          });
-        }, { threshold: 0.1 });
-        window.obz = observer
-        window.obz.observe(imageEl.current)
-      }
+    if (window.IntersectionObserver) {
 
-    
+      let observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+
+            entry.target.src = entry.target.dataset.src;
+            observer.unobserve(entry.target);
+          }
+        });
+      }, { threshold: 0.1 });
+      window.obz = observer
+      window.obz.observe(imageEl.current)
+    }
+
+
 
 
   }, []);
-  
+
 
 
   // function  addDefaultSrc(ev){
@@ -72,42 +57,63 @@ export default function ImageComponent(props) {
   //   }
 
   return (
-    <div>
-      <div style={{ position: 'relative', margin: 'auto' }}>
-
-        <a href={detailHost} target="_blank" rel="noreferrer">
-          <ImageListItem>
+    <AppContext.Consumer>{(({ selectedKeywords, parentKeyword }) => {
 
 
-            <img ref={imageEl} data-intersection="true" className="figure"
-
-              src={imagePlaceholder}
-              data-src={imageSource.trim()}
-              alt={props.title}
-              loading="lazy"
-            />
-          </ImageListItem>
-        </a>
-      </div>
-
-      <div style={{ display: 'flex', justifyContent: 'space-between', }}>
+      const { selectedSubcategory } = props
 
 
-        <img src={logo.image} height={logo.heigth} width={logo.width}  alt={props.title} />
+      const productTitle = props.title.split(' ').map(t => t.trim()).map((m, i) => {
 
-        <div>
-          <Typography variant="caption" style={{ textAlign: 'right', right: 2, flex: 5 }}>{props.price}TL</Typography>
+        const selectedKeywordMatch = selectedKeywords.find(f => f.keyword.includes(m.toLowerCase())) || selectedKeywords.map(t => { return { ...t, keyword: t.keyword.trim() } }).find(s => m.toLowerCase().includes(s))
+        const selectedSubcategoryMatch = selectedSubcategory.includes(m.toLowerCase()) || m.toLowerCase().includes(selectedSubcategory)
+
+
+        return <span key={i} style={{ fontWeight: (selectedKeywordMatch || selectedSubcategoryMatch) ? 800 : 300 }}>{m}{` `}
+
+
+        </span>
+      })
+      return <div>
+        <div style={{ position: 'relative', margin: 'auto' }}>
+
+          <a href={detailHost} target="_blank" rel="noreferrer">
+            <ImageListItem>
+
+
+              <img ref={imageEl} data-intersection="true" className="figure"
+
+                src={imagePlaceholder}
+                data-src={imageSource.trim()}
+                alt={props.title}
+                loading="lazy"
+              />
+            </ImageListItem>
+          </a>
         </div>
 
+        <div style={{ display: 'flex', justifyContent: 'space-between', }}>
+
+
+          <img src={logo.image} height={logo.heigth} width={logo.width} alt={props.title} />
+
+          <div>
+            <Typography variant="caption" style={{ textAlign: 'right', right: 2, flex: 5 }}>{props.price}TL</Typography>
+          </div>
+
+        </div>
+
+        <Typography variant="caption" display="block" gutterBottom>
+
+          {productTitle}
+        </Typography>
+
+        <Typography color='#9e9e9e' style={{ textAlign: 'right', fontSize: 9 }} variant="caption" display="block" gutterBottom>{minutes <= 59 ? minutes + ' dakika önce' : hour <= 24 ? hour + ' saat önce' : days <= 31 ? days + 'gün önce' : month + 'ay önce'}</Typography>
+
       </div>
+    })}
 
-      <Typography variant="caption" display="block" gutterBottom>
-
-        {productTitle}
-      </Typography>
-
-      <Typography color='#9e9e9e' style={{ textAlign: 'right', fontSize: 9 }} variant="caption" display="block" gutterBottom>{minutes <= 59 ? minutes + ' dakika önce' : hour <= 24 ? hour + ' saat önce' : days <= 31 ? days + 'gün önce' : month + 'ay önce'}</Typography>
-    </div>
+    </AppContext.Consumer>
 
   )
 
