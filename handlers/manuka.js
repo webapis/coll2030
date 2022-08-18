3cb7302742c9072ca5f7cc601494ec7ab9cecf62
webapis <60083568+webapis@users.noreply.github.com>
2022-08-18 13:12:15 +0500

    Update manuka.js

--- handlers/manuka.js
+++ handlers/manuka.js
@@ -2,31 +2,37 @@
 async function handler(page, context) {
     const { request: { userData: { subcategory, category, opts } } } = context
 
+
     const url = await page.url()
 
-    const language =await page.evaluate(()=>document.querySelector('span.forDesktop').innerHTML)
-    if(language==='English - EUR'){
-         let elem = await page.$('span.forDesktop')
-         await elem.hover()
-         await page.click('span[value="tr"]')
-         await page.waitForSelector('img[alt="Türkçe"]')
-    }
+    await page.waitForSelector('span.forDesktop')
+    let elem = await page.$('span.forDesktop')
+    await elem.hover()
+    await page.click('span[value="tr"]')
+    await page.waitForSelector('img[alt="Türkçe"]')
+    // const language =await page.evaluate(()=>document.querySelector('span.forDesktop').innerHTML)
+    // if(language==='English - EUR'){
+    //      let elem = await page.$('span.forDesktop')
+    //      await elem.hover()
+    //      await page.click('span[value="tr"]')
+    //     
+    // }
 
     debugger
     await page.waitForSelector('.fl.col-12.catalogWrapper')
     const products = await page.evaluate(() => window.PRODUCT_DATA)
-debugger
+    debugger
     debugger;
 
     const data = products.map(product => {
 
         const longImage = product.image
-        const title = product.name.replace(/I/g,'ı').replace(/İ/g,'i').toLowerCase()
+        const title = product.name.replace(/I/g, 'ı').replace(/İ/g, 'i').toLowerCase()
         const priceNew = product.total_sale_price.toString()
         const link = product.url
 
         return {
-            title: 'manuka ' + title.split(' ').map(m=> m.charAt(0).toUpperCase() + m.slice(1)).join(' '),
+            title: 'manuka ' + title.split(' ').map(m => m.charAt(0).toUpperCase() + m.slice(1)).join(' '),
             priceNew,
             imageUrl: longImage.substring(longImage.indexOf('https://www.manuka.com.tr') + 25),
             link,
