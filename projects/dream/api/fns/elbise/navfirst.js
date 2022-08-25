37d03c651c193faeee7966badbf6c39b020b0e71
webapis <60083568+webapis@users.noreply.github.com>
2022-08-25 11:43:26 +0500

    vv

--- projects/dream/api/fns/elbise/navfirst.js
+++ projects/dream/api/fns/elbise/navfirst.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
-const { navHandler } = require('../../../utils/navHandler')
+const { navHandler } = require('../../../../utils/navHandler')
 module.exports = (req, res) => {
 
 
