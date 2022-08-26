b88ff33987ce76a2c8e886efed64bae6651b0ff1
webapis <60083568+webapis@users.noreply.github.com>
2022-08-26 15:41:09 +0500

    v

--- urls/beymen.js
+++ urls/beymen.js
@@ -1,6 +1,6 @@
 const urls = [
   
-   // { url: 'https://www.beymen.com/kadin-giyim-gomlek-bluz-tunik-10380', subcategory: 'tunik', category: 'üst-giyim' },
+    { url: 'https://www.beymen.com/kadin-giyim-pantolon-sort-10043', subcategory: 'pantolon', category: 'alt-giyim' },
     {url:'https://www.beymen.com/kadin-giyim-elbise-10037',subcategory:'elbise',category:'üst-giyim',node:'dream',fn:'elbise'}
    
 ]
