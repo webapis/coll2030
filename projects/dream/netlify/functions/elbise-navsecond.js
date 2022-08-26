d1db8fe8317baf282243882f18c1df24c5b8aa6d
webapis <60083568+webapis@users.noreply.github.com>
2022-08-26 10:53:00 +0500

    Update elbise-navsecond.js

--- projects/dream/netlify/functions/elbise-navsecond.js
+++ projects/dream/netlify/functions/elbise-navsecond.js
@@ -1,14 +1,9 @@
 require('dotenv').config()
 
 const { navHandler } = require('../../utils/navHandler')
-exports.handler = async function (event, context)  {
-  console.log('hello from serverless')
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Hello World.." }),
-  };
+exports.handler = async function (event, context) {
 
 
- // navHandler({ req, res, subcategory: 'elbise', project: 'dream', keyOrder: '1' })
+return await  navHandler({ event, subcategory: 'elbise', project: 'dream', keyOrder: '1' })
 }
 
