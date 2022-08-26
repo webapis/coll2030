1e8066d90eb6b106363a3a5e26f6bf949bd8ae00
webapis <60083568+webapis@users.noreply.github.com>
2022-08-26 10:36:58 +0500

    v

--- projects/dream/netlify/functions/elbise/navfirst.js
+++ projects/dream/netlify/functions/elbise/navfirst.js
@@ -3,10 +3,7 @@ require('dotenv').config()
 const { navHandler } = require('../../../utils/navHandler')
 exports.handler = async function (event, context) {
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Hello World" }),
-  };
-//  navHandler({ req, res, subcategory: 'elbise', project: 'dream', keyOrder: '0' })
+
+return await  navHandler({ event, subcategory: 'elbise', project: 'dream', keyOrder: '0' })
 }
 
--- projects/dream/utils/navHandler.js
+++ projects/dream/utils/navHandler.js
@@ -3,19 +3,47 @@ var TAFFY = require('taffy');
 
 const path = require('path');
 
-function navHandler({ req, res, project, subcategory, keyOrder }) {
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    const { navindex } = req.query
-    const data = require(`../_files/${subcategory}/nav/${keyOrder}-keywords.json`)
-    var navkeywords = TAFFY(data);
-    debugger
+function navHandler({ event, project, subcategory, keyOrder }) {
 
-    console.log('navindex', navindex)
 
-    debugger
-    const { keywords, fn } = navkeywords().filter({ index: navindex }).get()[0]
-    debugger
-    res.status(200).json({ keywords, fn })
+    if (event.httpMethod === 'OPTIONS') {
+
+        const headers = {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
+        };
+
+        // To enable CORS
+        return {
+            statusCode: 200, // <-- Important!
+            headers,
+            body: 'This was not a POST request!'
+        };
+
+    } else if (event.httpMethod === 'GET') {
+        const { navindex } = event.queryStringParameters
+        const data = require(`../_files/${subcategory}/nav/${keyOrder}-keywords.json`)
+        var navkeywords = TAFFY(data);
+        debugger
+
+        console.log('navindex', navindex)
+
+        debugger
+        const { keywords, fn } = navkeywords().filter({ index: navindex }).get()[0]
+        debugger
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ keywords, fn })
+        }
+    } else {
+
+        return {
+            statusCode: 500,
+            body: 'unrecognized HTTP Method, must be one of GET/POST/PUT/DELETE/OPTIONS'
+        };
+    }
+
 }
 
 module.exports = { navHandler }
