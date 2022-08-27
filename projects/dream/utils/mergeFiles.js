9e7942683ee8f5a3d11fb4362813504c9f3f6e3a
webapis <60083568+webapis@users.noreply.github.com>
2022-08-27 12:11:58 +0500

    Update mergeFiles.js

--- projects/dream/utils/mergeFiles.js
+++ projects/dream/utils/mergeFiles.js
@@ -2,29 +2,29 @@
 console.log('--------------------------------------------------------------')
 const fs = require('fs')
 const path = require('path')
-const { walkSync } = require('./walkSync')
-console.log('--------------------------------------------------------------')
-    let obj = {}
-    walkSync(path.join(process.cwd(), 'data'), (filepath) => {
+// const { walkSync } = require('./walkSync')
+// console.log('--------------------------------------------------------------')
+//     let obj = {}
+//     walkSync(path.join(process.cwd(), 'data'), (filepath) => {
 
-        const dirName = path.dirname(filepath)
-        const data = JSON.parse(fs.readFileSync(filepath))
-        if (obj[dirName.replace(/[\\]/g, "-")] === undefined) {
-            obj[dirName.replace(/[\\]/g, "-")] = [data]
-        }
-        obj[dirName.replace(/[\\]/g, "-")] = [...obj[dirName.replace(/[\\]/g, "-")], data]
+//         const dirName = path.dirname(filepath)
+//         const data = JSON.parse(fs.readFileSync(filepath))
+//         if (obj[dirName.replace(/[\\]/g, "-")] === undefined) {
+//             obj[dirName.replace(/[\\]/g, "-")] = [data]
+//         }
+//         obj[dirName.replace(/[\\]/g, "-")] = [...obj[dirName.replace(/[\\]/g, "-")], data]
 
-    })
-    for (let o in obj) {
-        const s = o.split('-').reverse()
-        const marka = s[0]
-        const subcategory = s[1]
-        const project = s[2]
-        const data = obj[o]
-        fs.writeFileSync(path.join(process.cwd(), `projects/${project}/api/_files/data/${subcategory}/${marka}.json`), JSON.stringify(data))
-        debugger
+//     })
+//     for (let o in obj) {
+//         const s = o.split('-').reverse()
+//         const marka = s[0]
+//         const subcategory = s[1]
+//         const project = s[2]
+//         const data = obj[o]
+//         fs.writeFileSync(path.join(process.cwd(), `projects/${project}/api/_files/data/${subcategory}/${marka}.json`), JSON.stringify(data))
+//         debugger
 
-    }
+//     }
 
 
 
