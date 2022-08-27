4aa810f0a1818865a2797d635dac3b8e2e4b965c
webapis <60083568+webapis@users.noreply.github.com>
2022-08-27 13:08:45 +0500

    Update mergefiles.js

--- projects/dream/mergefiles.js
+++ projects/dream/mergefiles.js
@@ -7,24 +7,25 @@ console.log('--------------------------------------------------------------')
     let obj = {}
     walkSync(path.join(process.cwd(), 'data'), (filepath) => {
 
+        console.log('filepath',filepath)
         const dirName = path.dirname(filepath)
-        const data = JSON.parse(fs.readFileSync(filepath))
-        if (obj[dirName.replace(/[\\]/g, "-")] === undefined) {
-            obj[dirName.replace(/[\\]/g, "-")] = [data]
-        }
-        obj[dirName.replace(/[\\]/g, "-")] = [...obj[dirName.replace(/[\\]/g, "-")], data]
+        // const data = JSON.parse(fs.readFileSync(filepath))
+        // if (obj[dirName.replace(/[\\]/g, "-")] === undefined) {
+        //     obj[dirName.replace(/[\\]/g, "-")] = [data]
+        // }
+        // obj[dirName.replace(/[\\]/g, "-")] = [...obj[dirName.replace(/[\\]/g, "-")], data]
 
     })
-    for (let o in obj) {
-        const s = o.split('-').reverse()
-        const marka = s[0]
-        const subcategory = s[1]
-        const project = s[2]
-        const data = obj[o]
-        fs.writeFileSync(`./api/_files/data/${subcategory}/${marka}.json`), JSON.stringify(data)
-        debugger
+    // for (let o in obj) {
+    //     const s = o.split('-').reverse()
+    //     const marka = s[0]
+    //     const subcategory = s[1]
+    //     const project = s[2]
+    //     const data = obj[o]
+    //     fs.writeFileSync(`./api/_files/data/${subcategory}/${marka}.json`), JSON.stringify(data)
+    //     debugger
 
-    }
+    // }
 
 
 
