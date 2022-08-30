fe4dc18f45926b8fbc7c01d7b483b77885405ade
webapis <60083568+webapis@users.noreply.github.com>
2022-08-30 13:38:00 +0500

    c

--- main.js
+++ main.js
@@ -203,7 +203,7 @@ Apify.main(async () => {
                      //   const obj = JSON.parse(fs.readFileSync(`projects/${project}/data/${marka}/${subcategory}/${marka}/${id}.json`))
                       //  if (_.isEqual(obj, d) === false) {
                             debugger
-                          //  fs.unlinkSync(`projects/${project}/data/${marka}/${subcategory}/${marka}/${id}.json`)
+                            fs.unlinkSync(`projects/${project}/data/${marka}/${subcategory}/${marka}/${id}.json`)
                             fs.appendFileSync(`projects/${project}/data/${marka}/${subcategory}/${marka}/${id}.json`, JSON.stringify(d));
                             // const data = fs.readFileSync(`projects/${project}/data/${marka}/${subcategory}/${marka}/${id}.json`,{encoding:'utf-8'})
                             // const origin =JSON.parse(data)
