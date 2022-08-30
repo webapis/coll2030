cc05556ab5272d808578886a755be3965f64fc98
webapis <60083568+webapis@users.noreply.github.com>
2022-08-30 13:44:51 +0500

    Update defacto.js

--- urls/defacto.js
+++ urls/defacto.js
@@ -1,6 +1,6 @@
 const urls = [
   
-    // { url: 'https://www.defacto.com.tr/kadin-elbise', subcategory: 'elbise', category: 'üst-giyim',node:'dream',fn:'elbise' }
+     { url: 'https://www.defacto.com.tr/kadin-elbise', subcategory: 'elbise', category: 'üst-giyim',node:'dream',fn:'elbise' },
     {url:'https://www.defacto.com.tr/kadin-pantolon',subcategory: 'pantolon', category: 'alt-giyim',node:'dream',fn:'pantolon'} 
 ]
 
