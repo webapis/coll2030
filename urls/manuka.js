4d40e97206750becf8c2907a6f7e633667eef253
webapis <60083568+webapis@users.noreply.github.com>
2022-09-03 10:59:52 +0500

    d

--- urls/manuka.js
+++ urls/manuka.js
@@ -3,6 +3,7 @@ const urls = [
     { url: 'https://www.manuka.com.tr/elbise', subcategory: ['elbise'], category: 'üst-giyim',node:'dream' },
     { url: 'https://www.manuka.com.tr/abiye',  subcategory: ['elbise'], category: 'üst-giyim',node:'dream' },
     { url: 'https://www.manuka.com.tr/etek',   subcategory: ['etek'], category: 'alt-giyim',node:'dream' },
+    { url: 'https://www.manuka.com.tr/kadin-pantolon',   subcategory: ['pantolon'], category: 'alt-giyim',node:'dream' },
   
 ]
 
