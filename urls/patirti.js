73022c3922d5724477ae2dd3ebaaa01d282a6e46
webapis <60083568+webapis@users.noreply.github.com>
2022-09-03 11:06:41 +0500

    c

--- urls/patirti.js
+++ urls/patirti.js
@@ -1,7 +1,8 @@
 const urls = [
   
     { url: 'https://www.patirti.com/kadin-elbise', subcategory: ['elbise'], category: 'üst-giyim',node:'dream' },
-    { url: 'https://www.patirti.com/kadin-etek',    subcategory: ['etek'], category: 'alt-giyim',node:'dream' }
+    { url: 'https://www.patirti.com/kadin-etek',    subcategory: ['etek'], category: 'alt-giyim',node:'dream' },
+    { url: 'https://www.patirti.com/kadin-pantolon-salvar',    subcategory: ['pantolon'], category: 'alt-giyim',node:'dream' }
 ]
 
 module.exports={urls}
