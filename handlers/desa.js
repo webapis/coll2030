2870bc03b43a50103803dfd2734e9c4803c3fb21
webapis <60083568+webapis@users.noreply.github.com>
2022-09-03 10:41:42 +0500

    d

--- handlers/desa.js
+++ handlers/desa.js
@@ -1,88 +1,95 @@
 const { formatMoney } = require('accounting-js')
 async function handler(page, context) {
-    const { request: { userData: { subcategory, category, opts,node } } } = context
 
-    const url = await page.url()
+    try {
 
-    await page.waitForSelector('.list-content')
+        const { request: { userData: { subcategory, category, opts, node } } } = context
 
-    //
-    await manualScroll(page)
+        const url = await page.url()
 
-    const acceptcookies = await page.$('#personaWelcomePopupCloseBtn')
-    if (acceptcookies) {
-        await page.click('#personaWelcomePopupCloseBtn')
-    }
+        await page.waitForSelector('.list-content')
 
-    return new Promise((resolve, reject) => {
-        try {
-           let pause =false
-            let inv = setInterval(async () => {
-                console.log('pause',pause)
-                if(pause===false){
-                
-                const collected = await page.evaluate(() => document.querySelectorAll('.product-item-wrapper').length)
+        //
+        await manualScroll(page)
 
-                console.log('collected', collected)
-
-                const loadmorebtn = await page.$eval('.load-more-button', (element) => element.classList.contains('hidden'))
-                debugger
-                if (loadmorebtn === false) {
-                    debugger
-                    await page.click('.load-more-button')
-                    await manualScroll(page)
-                }
-                else {
-                    pause=true
-                  clearInterval(inv)
-                    debugger
-                    const data = await page.$$eval('.product-item-wrapper', (productCards, _subcategory, _category, _opts,_node) => {
-                        return productCards.map(productCard => {
-                            const priceNew = productCard.querySelector(".product-sale-price") ? productCard.querySelector(".product-sale-price").textContent.replace(/\n/g, '').trim().replace('₺', '').replace('TL', '').trim() : productCard.outerHTML
-                            const longlink = productCard.querySelector('.product-name a') ? productCard.querySelector('.product-name a').href : productCard.outerHTML
-                            const link = longlink.substring(longlink.indexOf("https://www.desa.com.tr/") + 24)
-                            const longImgUrl = productCard.querySelector('[data-src]') ? productCard.querySelector('[data-src]').getAttribute('data-src') : productCard.outerHTML
-                            const imageUrlshort = longImgUrl && longImgUrl.substring(longImgUrl.indexOf('https://cdn-ayae.akinon.net/') + 28)
-                            const title = productCard.querySelector(".product-name a") ? productCard.querySelector(".product-name a").innerHTML : productCard.outerHTML
-                            return {
-                                title: 'desa ' + title,//,+ (_opts.keyword ? (title.toLowerCase().includes(_opts.keyword) ? '' : ' ' + _opts.keyword) : ''),
-                                priceNew,
-                                imageUrl: imageUrlshort,
-                                link,
-                                timestamp: Date.now(),
-                                marka: 'desa',
-                               // subcategory: _subcategory,
-                                category: _category,
-                                node: _node
-                            }
-                        }).filter(f => f.imageUrl !== null)
-                    }, subcategory, category, opts,node)
-
-
-                    console.log('data length_____', data.length, 'url:', url)
-                    debugger
-                    const withSub = data.map(m => {
-                        const { title } = m
-                        const subcatmatches = subcategory.filter(f => title.toLowerCase().includes(f))
-                        const subcat = subcatmatches.length > 0 ? subcatmatches[0] : subcategory[0]
-                        debugger
-                        return { ...m, subcategory: subcat }
-                    })
-                
-               
-                    return resolve(withSub.map((m) => {
-                        return { ...m, priceNew: formatMoney(parseFloat(m.priceNew), { symbol: "", precision: 2, thousand: ".", decimal: "," }) }
-                    }))
-                 
-                }
-            }
-            }, 200)
-        
-        } catch (error) {
-            debugger
-           // return reject(error)
+        const acceptcookies = await page.$('#personaWelcomePopupCloseBtn')
+        if (acceptcookies) {
+            await page.click('#personaWelcomePopupCloseBtn')
         }
-    })
+        return new Promise((resolve, reject) => {
+            try {
+                let pause = false
+                let inv = setInterval(async () => {
+                    console.log('pause', pause)
+                    if (pause === false) {
+
+                        const collected = await page.evaluate(() => document.querySelectorAll('.product-item-wrapper').length)
+
+                        console.log('collected', collected)
+
+                        const loadmorebtn = await page.$eval('.load-more-button', (element) => element.classList.contains('hidden'))
+                        debugger
+                        if (loadmorebtn === false) {
+                            debugger
+                            await page.click('.load-more-button')
+                            await manualScroll(page)
+                        }
+                        else {
+                            pause = true
+                            clearInterval(inv)
+                            debugger
+                            const data = await page.$$eval('.product-item-wrapper', (productCards, _subcategory, _category, _opts, _node) => {
+                                return productCards.map(productCard => {
+                                    const priceNew = productCard.querySelector(".product-sale-price") ? productCard.querySelector(".product-sale-price").textContent.replace(/\n/g, '').trim().replace('₺', '').replace('TL', '').trim() : productCard.outerHTML
+                                    const longlink = productCard.querySelector('.product-name a') ? productCard.querySelector('.product-name a').href : productCard.outerHTML
+                                    const link = longlink.substring(longlink.indexOf("https://www.desa.com.tr/") + 24)
+                                    const longImgUrl = productCard.querySelector('[data-src]') ? productCard.querySelector('[data-src]').getAttribute('data-src') : productCard.outerHTML
+                                    const imageUrlshort = longImgUrl && longImgUrl.substring(longImgUrl.indexOf('https://cdn-ayae.akinon.net/') + 28)
+                                    const title = productCard.querySelector(".product-name a") ? productCard.querySelector(".product-name a").innerHTML : productCard.outerHTML
+                                    return {
+                                        title: 'desa ' + title,//,+ (_opts.keyword ? (title.toLowerCase().includes(_opts.keyword) ? '' : ' ' + _opts.keyword) : ''),
+                                        priceNew,
+                                        imageUrl: imageUrlshort,
+                                        link,
+                                        timestamp: Date.now(),
+                                        marka: 'desa',
+                                        // subcategory: _subcategory,
+                                        category: _category,
+                                        node: _node
+                                    }
+                                }).filter(f => f.imageUrl !== null)
+                            }, subcategory, category, opts, node)
+
+
+                            console.log('data length_____', data.length, 'url:', url)
+                            debugger
+                            const withSub = data.map(m => {
+                                const { title } = m
+                                const subcatmatches = subcategory.filter(f => title.toLowerCase().includes(f))
+                                const subcat = subcatmatches.length > 0 ? subcatmatches[0] : subcategory[0]
+                                debugger
+                                return { ...m, subcategory: subcat }
+                            })
+
+
+                            return resolve(withSub.map((m) => {
+                                return { ...m, priceNew: formatMoney(parseFloat(m.priceNew), { symbol: "", precision: 2, thousand: ".", decimal: "," }) }
+                            }))
+
+                        }
+                    }
+                }, 200)
+
+            } catch (error) {
+                debugger
+                console.log('error 1', error)
+                return reject(error)
+            }
+        })
+    } catch (error) {
+        console.log('error 2', error)
+
+    }
 }
 
 
--- urls/lcwaikiki.js
+++ urls/lcwaikiki.js
@@ -1,7 +1,8 @@
 const urls = [
     
     { url: 'https://www.lcwaikiki.com/tr-TR/TR/kategori/kadin/elbise', subcategory: ['elbise'], category: 'üst-giyim',node:'dream'},
-    { url: 'https://www.lcwaikiki.com/tr-TR/TR/kategori/kadin/etek', subcategory: ['etek'], category: 'alt-giyim',node:'dream'}
+    { url: 'https://www.lcwaikiki.com/tr-TR/TR/kategori/kadin/etek', subcategory: ['etek'], category: 'alt-giyim',node:'dream'},
+    { url: 'https://www.lcwaikiki.com/tr-TR/TR/kategori/kadin/pantolon', subcategory: ['pantolon'], category: 'alt-giyim',node:'dream'}
 
 
 ]
