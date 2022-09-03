e3945b5d7020b9060481decb4614e86e24c44ff0
webapis <60083568+webapis@users.noreply.github.com>
2022-09-03 10:57:04 +0500

    c

--- urls/mango.js
+++ urls/mango.js
@@ -1,7 +1,8 @@
 const urls = [
   
-    { url: 'https://shop.mango.com/tr/kadin/elbise-ve-tulum_c20249297', subcategory: ['elbise'], category: 'üst-giyim',node:'dream' },
-    { url: 'https://shop.mango.com/tr/kadin/etek_c20673898', subcategory: ['etek'], category: 'alt-giyim',node:'dream' }
+    { url: 'https://shop.mango.com/tr/kadin/elbise-ve-tulum_c20249297',     subcategory: ['elbise'], category: 'üst-giyim',node:'dream' },
+    { url: 'https://shop.mango.com/tr/kadin/etek_c20673898',                subcategory: ['etek'], category: 'alt-giyim',node:'dream' },
+    { url: 'https://shop.mango.com/tr/kadin/pantolon_c52748027',            subcategory: ['pantolon'], category: 'alt-giyim',node:'dream' }
 
   
 ]
