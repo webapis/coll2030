faad95f49f5e99387d3f6225d14abe10e3ce239b
webapis <60083568+webapis@users.noreply.github.com>
2022-09-05 15:11:53 +0500

    Update hm.js

--- urls/hm.js
+++ urls/hm.js
@@ -9,7 +9,7 @@ const urls = [
      { url: 'https://www2.hm.com/tr_tr/kadin/urune-gore-satin-al/gomlek-bluz.html', subcategory: ['bluz','gömlek'], category: 'üst-giyim', node: 'dream' },
     // { url: 'https://www2.hm.com/tr_tr/kadin/urune-gore-satin-al/blazer-yelek.html', subcategory: ['ceket'], category: 'dış-giyim', node: 'dream' },
      { url: 'https://www2.hm.com/tr_tr/kadin/urune-gore-satin-al/ceket-kaban.html', subcategory: ['ceket','kaban'], category: 'dış-giyim', node: 'dream' },
-    // { url: 'https://www2.hm.com/tr_tr/kadin/urune-gore-satin-al/sort.html', subcategory: ['şort'], category: 'alt-giyim', node: 'dream' },
+     { url: 'https://www2.hm.com/tr_tr/kadin/urune-gore-satin-al/sort.html', subcategory: ['şort'], category: 'alt-giyim', node: 'dream' },
 ]
 
 module.exports={urls}
