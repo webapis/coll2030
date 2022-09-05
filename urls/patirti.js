e00014165d791cd9b4ad71edb5697089c8cf3d22
webapis <60083568+webapis@users.noreply.github.com>
2022-09-05 14:56:28 +0500

    v

--- urls/patirti.js
+++ urls/patirti.js
@@ -2,7 +2,19 @@ const urls = [
   
     { url: 'https://www.patirti.com/kadin-elbise', subcategory: ['elbise'], category: 'üst-giyim',node:'dream' },
     { url: 'https://www.patirti.com/kadin-etek',    subcategory: ['etek'], category: 'alt-giyim',node:'dream' },
-    { url: 'https://www.patirti.com/kadin-pantolon-salvar',    subcategory: ['pantolon'], category: 'alt-giyim',node:'dream' }
+    { url: 'https://www.patirti.com/kadin-pantolon-salvar',    subcategory: ['pantolon'], category: 'alt-giyim',node:'dream' },
+
+
+    { url: 'https://www.patirti.com/kadin-sweat', subcategory: ['sweatshirt'], category: 'üst-giyim', node: 'dream' },
+    { url: 'https://www.patirti.com/kadin-hirka', subcategory: ['hırka'], category: 'üst-giyim', node: 'dream' },
+    { url: 'https://www.patirti.com/kadin-bluz', subcategory: ['bluz'], category: 'üst-giyim', node: 'dream' },
+    { url: 'https://www.patirti.com/kadin-tisort', subcategory: ['tişört'], category: 'üst-giyim', node: 'dream' },
+    { url: 'https://www.patirti.com/kadin-gomlek', subcategory: ['gömlek'], category: 'üst-giyim', node: 'dream' },
+    { url: 'https://www.patirti.com/kadin-tulumlar', subcategory: ['takım', 'tulum'], category: 'üst-giyim', node: 'dream' },
+    { url: 'https://www.patirti.com/kadin-mont-ceket', subcategory: ['ceket','mont'], category: 'dış-giyim', node: 'dream' },
+    { url: 'https://www.patirti.com/atlet-2', subcategory: ['atlet'], category: 'üst-giyim', node: 'dream' },
+    { url: 'https://www.patirti.com/kadin-tayt', subcategory: ['tayt'], category: 'alt-giyim', node: 'dream' },
+    { url: 'https://www.patirti.com/kadin-sort-kapri', subcategory: ['şort'], category: 'alt-giyim', node: 'dream' },
 ]
 
 module.exports={urls}
