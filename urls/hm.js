eaa2f4b99bf5a4b4a08d6c1e7e49055ed7841ee6
webapis <60083568+webapis@users.noreply.github.com>
2022-09-05 14:28:55 +0500

    Update hm.js

--- urls/hm.js
+++ urls/hm.js
@@ -5,11 +5,11 @@ const urls = [
     { url: 'https://www2.hm.com/tr_tr/kadin/urune-gore-satin-al/etek.html', subcategory:['etek'], category: 'alt-giyim',node:'dream' },
     { url: 'https://www2.hm.com/tr_tr/kadin/urune-gore-satin-al/pantolon.html', subcategory:['pantolon'], category: 'alt-giyim',node:'dream' },
     { url: 'https://www2.hm.com/tr_tr/kadin/urune-gore-satin-al/kapusonlular-sweatshirtler.html', subcategory: ['sweatshirt'], category: 'üst-giyim', node: 'dream' },
-    { url: 'https://www2.hm.com/tr_tr/kadin/urune-gore-satin-al/hirka-kazak.html', subcategory: ['hırka'], category: 'üst-giyim', node: 'dream' },
-    { url: 'https://www2.hm.com/tr_tr/kadin/urune-gore-satin-al/gomlek-bluz.html', subcategory: ['bluz','gömlek'], category: 'üst-giyim', node: 'dream' },
-    { url: 'https://www2.hm.com/tr_tr/kadin/urune-gore-satin-al/blazer-yelek.html', subcategory: ['ceket'], category: 'dış-giyim', node: 'dream' },
-    { url: 'https://www2.hm.com/tr_tr/kadin/urune-gore-satin-al/ceket-kaban.html', subcategory: ['ceket','kaban'], category: 'dış-giyim', node: 'dream' },
-    { url: 'https://www2.hm.com/tr_tr/kadin/urune-gore-satin-al/sort.html', subcategory: ['şort'], category: 'alt-giyim', node: 'dream' },
+    // { url: 'https://www2.hm.com/tr_tr/kadin/urune-gore-satin-al/hirka-kazak.html', subcategory: ['hırka'], category: 'üst-giyim', node: 'dream' },
+    // { url: 'https://www2.hm.com/tr_tr/kadin/urune-gore-satin-al/gomlek-bluz.html', subcategory: ['bluz','gömlek'], category: 'üst-giyim', node: 'dream' },
+    // { url: 'https://www2.hm.com/tr_tr/kadin/urune-gore-satin-al/blazer-yelek.html', subcategory: ['ceket'], category: 'dış-giyim', node: 'dream' },
+    // { url: 'https://www2.hm.com/tr_tr/kadin/urune-gore-satin-al/ceket-kaban.html', subcategory: ['ceket','kaban'], category: 'dış-giyim', node: 'dream' },
+    // { url: 'https://www2.hm.com/tr_tr/kadin/urune-gore-satin-al/sort.html', subcategory: ['şort'], category: 'alt-giyim', node: 'dream' },
 ]
 
 module.exports={urls}
