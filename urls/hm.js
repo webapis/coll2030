30e9aa47ed19d6c4d44a48f9c2dc998da25d32a8
webapis <60083568+webapis@users.noreply.github.com>
2022-09-05 15:03:39 +0500

    v

--- urls/hm.js
+++ urls/hm.js
@@ -7,8 +7,8 @@ const urls = [
     { url: 'https://www2.hm.com/tr_tr/kadin/urune-gore-satin-al/kapusonlular-sweatshirtler.html', subcategory: ['sweatshirt'], category: 'üst-giyim', node: 'dream' },
      { url: 'https://www2.hm.com/tr_tr/kadin/urune-gore-satin-al/hirka-kazak.html', subcategory: ['hırka'], category: 'üst-giyim', node: 'dream' },
      { url: 'https://www2.hm.com/tr_tr/kadin/urune-gore-satin-al/gomlek-bluz.html', subcategory: ['bluz','gömlek'], category: 'üst-giyim', node: 'dream' },
-     { url: 'https://www2.hm.com/tr_tr/kadin/urune-gore-satin-al/blazer-yelek.html', subcategory: ['ceket'], category: 'dış-giyim', node: 'dream' },
-    // { url: 'https://www2.hm.com/tr_tr/kadin/urune-gore-satin-al/ceket-kaban.html', subcategory: ['ceket','kaban'], category: 'dış-giyim', node: 'dream' },
+    // { url: 'https://www2.hm.com/tr_tr/kadin/urune-gore-satin-al/blazer-yelek.html', subcategory: ['ceket'], category: 'dış-giyim', node: 'dream' },
+     { url: 'https://www2.hm.com/tr_tr/kadin/urune-gore-satin-al/ceket-kaban.html', subcategory: ['ceket','kaban'], category: 'dış-giyim', node: 'dream' },
     // { url: 'https://www2.hm.com/tr_tr/kadin/urune-gore-satin-al/sort.html', subcategory: ['şort'], category: 'alt-giyim', node: 'dream' },
 ]
 
