518b8400c4a753f69b74bac56c6847b72c22704e
webapis <60083568+webapis@users.noreply.github.com>
2022-09-06 08:30:05 +0500

    Update tozlu.js

--- urls/tozlu.js
+++ urls/tozlu.js
@@ -4,6 +4,21 @@ const urls = [
 
     { url: 'https://www.tozlu.com/TR/kadin-etek',   subcategory: ['etek'], category: 'alt-giyim',node:'dream' },
     { url: 'https://www.tozlu.com/TR/kadin-pantolon',   subcategory: ['pantolon'], category: 'alt-giyim',node:'dream' },
+
+
+
+    { url: 'https://www.tozlu.com/TR/kadin-sweatshirt', subcategory: ['sweatshirt'], category: 'üst-giyim', node: 'dream' },
+    { url: 'https://www.tozlu.com/TR/kadin-hirka', subcategory: ['hırka'], category: 'üst-giyim', node: 'dream' },
+    { url: 'https://www.tozlu.com/TR/kadin-bluz', subcategory: ['bluz'], category: 'üst-giyim', node: 'dream' },
+    { url: 'https://www.tozlu.com/TR/kadin-tshirt', subcategory: ['tişört'], category: 'üst-giyim', node: 'dream' },
+    { url: 'https://www.tozlu.com/TR/kadin-gomlek', subcategory: ['gömlek'], category: 'üst-giyim', node: 'dream' },
+    { url: 'https://www.tozlu.com/TR/kadin-giyim-tulum-salopet', subcategory: ['takım', 'tulum'], category: 'üst-giyim', node: 'dream' },
+    { url: 'https://www.tozlu.com/TR/kadin-ceket', subcategory: ['ceket'], category: 'dış-giyim', node: 'dream' },
+    { url: 'https://www.tozlu.com/TR/kadin-mevsimlik-mont', subcategory: ['mont'], category: 'dış-giyim', node: 'dream' },
+    { url: 'https://www.tozlu.com/TR/kadin-trenckot', subcategory: ['trençkot'], category: 'dış-giyim', node: 'dream' },
+    { url: 'https://www.tozlu.com/TR/kadin-tunik', subcategory: ['tunik'], category: 'üst-giyim', node: 'dream' },
+    { url: 'https://www.tozlu.com/TR/kadin-tayt', subcategory: ['tayt'], category: 'alt-giyim', node: 'dream' },
+    { url: 'https://www.tozlu.com/TR/kadin-giyim-sort-kapri', subcategory: ['şort'], category: 'alt-giyim', node: 'dream' },
 ]
 
 module.exports={urls}
