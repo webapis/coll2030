19c157cf92fbf17360c67b650576017e6041e072
webapis <60083568+webapis@users.noreply.github.com>
2022-09-06 09:13:01 +0500

    v

--- urls/yargici.js
+++ urls/yargici.js
@@ -6,7 +6,14 @@ const urls = [
     { url: 'https://www.yargici.com/yeni-sezon-pantolon', subcategory:['pantolon'], category: 'alt-giyim',node:'dream' },
 
 
+    { url: 'https://www.yargici.com/yeni-sezon-hirka', subcategory: ['hırka'], category: 'üst-giyim', node: 'dream' },
 
+    { url: 'https://www.yargici.com/yeni-sezon-t-shirt', subcategory: ['tişört'], category: 'üst-giyim', node: 'dream' },
+    { url: 'https://www.yargici.com/yeni-sezon-gomlek-bluz', subcategory: ['bluz','gömlek'], category: 'üst-giyim', node: 'dream' },
+    { url: 'https://www.yargici.com/yeni-sezon-ceket', subcategory: ['ceket'], category: 'dış-giyim', node: 'dream' },
+    { url: 'https://www.yargici.com/palto', subcategory: ['kaban'], category: 'dış-giyim', node: 'dream' },
+    { url: 'https://www.yargici.com/mont', subcategory: ['mont'], category: 'dış-giyim', node: 'dream' },
+    { url: 'https://www.yargici.com/bermuda-sort', subcategory: ['şort'], category: 'alt-giyim', node: 'dream' },
 ]
 
 module.exports={urls}
