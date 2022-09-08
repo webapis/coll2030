8080807b9f855c2726ac2e639b16882b7ec6747c
webapis <60083568+webapis@users.noreply.github.com>
2022-09-08 15:24:06 +0500

    Update defacto.js

--- urls/defacto.js
+++ urls/defacto.js
@@ -1,27 +1,27 @@
 const urls = [
 
-//    { url: 'https://www.defacto.com.tr/kadin-elbise', subcategory: ['elbise'], category: 'üst-giyim', node: 'dream' },
-//     { url: 'https://www.defacto.com.tr/kadin-pantolon', subcategory: ['pantolon'], category: 'alt-giyim', node: 'dream' },
+   { url: 'https://www.defacto.com.tr/kadin-elbise', subcategory: ['elbise'], category: 'üst-giyim', node: 'dream' },
+    { url: 'https://www.defacto.com.tr/kadin-pantolon', subcategory: ['pantolon'], category: 'alt-giyim', node: 'dream' },
    { url: 'https://www.defacto.com.tr/kadin-etek', subcategory: ['etek'], category: 'alt-giyim', node: 'dream' },
 
-    // { url: 'https://www.defacto.com.tr/kadin-gomlek', subcategory: ['gömlek'], category: 'üst-giyim', node: 'dream' },
-    // { url: 'https://www.defacto.com.tr/kadin-hirka', subcategory: ['hırka'], category: 'üst-giyim', node: 'dream' },
-    // { url: 'https://www.defacto.com.tr/kadin-t-shirt', subcategory: ['tişört'], category: 'üst-giyim', node: 'dream' },
-    // { url: 'https://www.defacto.com.tr/kadin-sweatshirt', subcategory: ['sweatshirt'], category: 'üst-giyim', node: 'dream' },
+    { url: 'https://www.defacto.com.tr/kadin-gomlek', subcategory: ['gömlek'], category: 'üst-giyim', node: 'dream' },
+    { url: 'https://www.defacto.com.tr/kadin-hirka', subcategory: ['hırka'], category: 'üst-giyim', node: 'dream' },
+    { url: 'https://www.defacto.com.tr/kadin-t-shirt', subcategory: ['tişört'], category: 'üst-giyim', node: 'dream' },
+    { url: 'https://www.defacto.com.tr/kadin-sweatshirt', subcategory: ['sweatshirt'], category: 'üst-giyim', node: 'dream' },
 
-    // { url: 'https://www.defacto.com.tr/kadin-bluz', subcategory: ['bluz'], category: 'üst-giyim', node: 'dream' },
+    { url: 'https://www.defacto.com.tr/kadin-bluz', subcategory: ['bluz'], category: 'üst-giyim', node: 'dream' },
 
-    // { url: 'https://www.defacto.com.tr/kadin-sort', subcategory: ['şort'], category: 'alt-giyim', node: 'dream' },
+    { url: 'https://www.defacto.com.tr/kadin-sort', subcategory: ['şort'], category: 'alt-giyim', node: 'dream' },
 
-    // { url: 'https://www.defacto.com.tr/kadin-ceket', subcategory: ['ceket'], category: 'üst-giyim', node: 'dream' },
+    { url: 'https://www.defacto.com.tr/kadin-ceket', subcategory: ['ceket'], category: 'üst-giyim', node: 'dream' },
 
-    // { url: 'https://www.defacto.com.tr/kadin-tunik', subcategory: ['tunik'], category: 'üst-giyim', node: 'dream' },
+    { url: 'https://www.defacto.com.tr/kadin-tunik', subcategory: ['tunik'], category: 'üst-giyim', node: 'dream' },
 
-    // { url: 'https://www.defacto.com.tr/kadin-tayt', subcategory: ['tayt'], category: 'alt-giyim', node: 'dream' },
+    { url: 'https://www.defacto.com.tr/kadin-tayt', subcategory: ['tayt'], category: 'alt-giyim', node: 'dream' },
     
-    // { url: 'https://www.defacto.com.tr/kadin-dis-giyim', subcategory: ['yağmurluk', 'mont',  'trençkot', 'kaban','ceket'], category: 'dış-giyim', node: 'dream' }, 
-    // { url: 'https://www.defacto.com.tr/kadin-atlet', subcategory: ['atlet'], category: 'üst-giyim', node: 'dream' },
-    // { url: 'https://www.defacto.com.tr/kadin-esofman-alti', subcategory: ['eşofman altı'], category: 'alt-giyim', node: 'dream' },
+    { url: 'https://www.defacto.com.tr/kadin-dis-giyim', subcategory: ['yağmurluk', 'mont',  'trençkot', 'kaban','ceket'], category: 'dış-giyim', node: 'dream' }, 
+    { url: 'https://www.defacto.com.tr/kadin-atlet', subcategory: ['atlet'], category: 'üst-giyim', node: 'dream' },
+    { url: 'https://www.defacto.com.tr/kadin-esofman-alti', subcategory: ['eşofman altı'], category: 'alt-giyim', node: 'dream' },
 ]
 
 module.exports = { urls }
