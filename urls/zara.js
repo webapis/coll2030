4deca140885a92c6805978823ebce19d2b564bef
webapis <60083568+webapis@users.noreply.github.com>
2022-09-10 14:32:29 +0500

    Update zara.js

--- urls/zara.js
+++ urls/zara.js
@@ -2,7 +2,7 @@ const urls = [
   
     { url: 'https://www.zara.com/tr/tr/kadin-elbiseler-l1066.html', subcategory: ['tulum','elbise'], category: 'üst-giyim',node:'dream' },
     { url: 'https://www.zara.com/tr/tr/kadin-etekler-l1299.html',   subcategory: ['etek'], category: 'alt-giyim',node:'dream' },
-    {url:'https://www.zara.com/tr/tr/kadin-pantolonlar-l1335.html', subcategory:'pantolon',category:'alt-giyim',node:'dream'},
+    {url:'https://www.zara.com/tr/tr/kadin-pantolonlar-l1335.html', subcategory:['pantolon'],category:'alt-giyim',node:'dream'},
   
     { url: 'https://www.zara.com/tr/tr/kadin-sweatshirtler-l1320.html', subcategory: ['sweatshirt'], category: 'üst-giyim', node: 'dream' },
 
