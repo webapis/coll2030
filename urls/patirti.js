111cd0ff45e83b42dc05944efafad47a082d4664
webapis <60083568+webapis@users.noreply.github.com>
2022-09-13 15:58:01 +0500

    Update patirti.js

--- urls/patirti.js
+++ urls/patirti.js
@@ -8,11 +8,10 @@ const urls = [
     { url: 'https://www.patirti.com/kadin-sweat',              subcategory: ['sweatshirt'], category: 'üst-giyim', node: 'dream' },
     { url: 'https://www.patirti.com/kadin-hirka',              subcategory: ['hırka'], category: 'üst-giyim', node: 'dream' },
     { url: 'https://www.patirti.com/kadin-bluz',               subcategory: ['bluz'], category: 'üst-giyim', node: 'dream' },
-    { url: 'https://www.patirti.com/kadin-tisort',             subcategory: ['tişört'], category: 'üst-giyim', node: 'dream' },
+    { url: 'https://www.patirti.com/kadin-t-shirt',             subcategory: ['tişört'], category: 'üst-giyim', node: 'dream' },
     { url: 'https://www.patirti.com/kadin-gomlek',             subcategory: ['gömlek'], category: 'üst-giyim', node: 'dream' },
-    { url: 'https://www.patirti.com/kadin-tulumlar',           subcategory: [ 'tulum'], category: 'üst-giyim', node: 'dream' },
+    { url: 'https://www.patirti.com/kadin-tulum-ve-salopet',   subcategory: [ 'tulum'], category: 'üst-giyim', node: 'dream' },
     { url: 'https://www.patirti.com/kadin-mont-ceket',         subcategory: ['ceket','mont'], category: 'dış-giyim', node: 'dream' },
-    { url: 'https://www.patirti.com/atlet-2',                  subcategory: ['atlet'], category: 'üst-giyim', node: 'dream' },
     { url: 'https://www.patirti.com/kadin-tayt',               subcategory: ['tayt'], category: 'alt-giyim', node: 'dream' },
     { url: 'https://www.patirti.com/kadin-sort-kapri',         subcategory: ['şort'], category: 'alt-giyim', node: 'dream' },
 ]
