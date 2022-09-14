90c5984c29a2744574e9be3108c96ff14f427591
webapis <60083568+webapis@users.noreply.github.com>
2022-09-14 11:51:12 +0500

    Update patirti.js

--- urls/patirti.js
+++ urls/patirti.js
@@ -6,7 +6,7 @@ const urls = [
 
 
     { url: 'https://www.patirti.com/kadin-sweat',              subcategory: ['sweatshirt'], category: 'üst-giyim', node: 'dream' },
-    { url: 'https://www.patirti.com/kadin-hirka',              subcategory: ['hırka'], category: 'üst-giyim', node: 'dream' },
+    { url: 'https://www.patirti.com/kadin-hirka',              subcategory: ['pareo','ceket','hırka'], category: 'üst-giyim', node: 'dream' },
     { url: 'https://www.patirti.com/kadin-bluz',               subcategory: ['bluz'], category: 'üst-giyim', node: 'dream' },
     { url: 'https://www.patirti.com/kadin-t-shirt',             subcategory: ['tişört'], category: 'üst-giyim', node: 'dream' },
     { url: 'https://www.patirti.com/kadin-gomlek',             subcategory: ['gömlek'], category: 'üst-giyim', node: 'dream' },
