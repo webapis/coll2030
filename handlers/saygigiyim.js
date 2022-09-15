fbbbf873582e618348736e9db5dbd8a293020653
webapis <60083568+webapis@users.noreply.github.com>
2022-09-15 10:31:02 +0500

    Update saygigiyim.js

--- handlers/saygigiyim.js
+++ handlers/saygigiyim.js
@@ -1,82 +1,89 @@
 
 async function handler(page, context) {
-    const { request: { userData: { subcategory, category,node } } } = context
+    const { request: { userData: { subcategory, category, node } } } = context
 
     const url = await page.url()
 
-    await page.waitForSelector('#ProductPageProductList')
-    // onetrust-accept-btn-handler
 
+    // onetrust-accept-btn-handler
+    const productExist = await page.$('.appliedFilter.FiltrelemeUrunAdet span')
     const acceptcookies = await page.$('.seg-popup-close')
     if (acceptcookies) {
         await page.click('.seg-popup-close')
     }
+    if (productExist) {
+        return new Promise((resolve, reject) => {
+            try {
 
-    return new Promise((resolve, reject) => {
-        try {
-
-            let inv = setInterval(async () => {
-                // const { loaded, remained } = await page.$eval('.load-more-heading', el => {
-                //     return { loaded: parseInt(el.getAttribute('data-items-shown')), remained: parseInt(el.getAttribute('data-total')) }
-                // })
-                const totalProducts = await page.evaluate(() => parseInt(document.querySelector('.appliedFilter.FiltrelemeUrunAdet span').innerHTML.replace(/[^\d]/g, '')))
-                const collected = await page.evaluate(() => document.querySelectorAll('#ProductPageProductList .productItem').length)
-
-                console.log('collected', collected)
-
-                if (totalProducts > collected) {
-
-                    //  await page.click('.button.js-load-more')
-                    await manualScroll(page)
-
-                } else {
-                    clearInterval(inv)
-
-                    const data = await page.$$eval('.productItem', (productCards, _subcategory, _category,_node) => {
-                        return productCards.map(productCard => {
-                            const priceNew = productCard.querySelector('.discountPrice span').textContent.replace(/\n/g, '').trim().replace('₺', '').replace('TL', '').trim()
-                            const longlink = productCard.querySelector('.productName a').href
-                            const link = longlink.substring(longlink.indexOf("https://www.saygigiyim.com/") + 27)
-                            const longImgUrl = productCard.querySelector('img[data-original]') &&  productCard.querySelector('img[data-original]').getAttribute('data-original')
-                            //const imageUrlshort = longImgUrl&& longImgUrl.substring(longImgUrl.indexOf('https://www.quzu.com.tr/') + 24)
-                            const title = productCard.querySelector('.productName a').innerHTML
-
-                            return {
-                                title: 'saygigiyim ' + title.replace(/İ/g,'i').toLowerCase(),
-                                priceNew,//:priceNew.replace('.','').replace(',','.').trim(),
-                                imageUrl: longImgUrl,
-                                link,
-                                timestamp: Date.now(),
-                                marka: 'saygigiyim',
-                              //  subcategory: _subcategory,
-                                category: _category,
-                                node: _node
-                            }
-                        }).filter(f => f.imageUrl !== null)
-                    }, subcategory, category,node)
-
-          
-                    console.log('data length_____', data.length, 'url:', url)
+                let inv = setInterval(async () => {
+                    // const { loaded, remained } = await page.$eval('.load-more-heading', el => {
+                    //     return { loaded: parseInt(el.getAttribute('data-items-shown')), remained: parseInt(el.getAttribute('data-total')) }
+                    // })
+                    const totalProducts = await page.evaluate(() => parseInt(document.querySelector('.appliedFilter.FiltrelemeUrunAdet span').innerHTML.replace(/[^\d]/g, '')))
+                    const collected = await page.evaluate(() => document.querySelectorAll('#ProductPageProductList .productItem').length)
                     debugger
-                    const withSub = data.map(m => {
-                        const { title } = m
-                        const subcatmatches = subcategory.filter(f => title.toLowerCase().includes(f))
-                        const subcat = subcatmatches.length > 0 ? subcatmatches[0] : subcategory[subcategory.length-1]
+                    console.log('collected', collected)
+
+                    if (totalProducts > collected) {
                         debugger
-                        return { ...m, subcategory: subcat }
-                    })
-                   
-                    return resolve(withSub)
+                        //  await page.click('.button.js-load-more')
+                        await manualScroll(page)
 
-                }
+                    } else {
+                        clearInterval(inv)
 
-            }, 170)
-            // clearInterval(inv)
-        } catch (error) {
-            debugger
-            return reject(error)
-        }
-    })
+                        const data = await page.$$eval('.productItem', (productCards, _subcategory, _category, _node) => {
+                            return productCards.map(productCard => {
+                                const priceNew = productCard.querySelector('.discountPrice span').textContent.replace(/\n/g, '').trim().replace('₺', '').replace('TL', '').trim()
+                                const longlink = productCard.querySelector('.detailLink').href
+                                const link = longlink.substring(longlink.indexOf("https://www.saygigiyim.com/") + 27)
+                                const longImgUrl = productCard.querySelector('img[data-original]') && productCard.querySelector('img[data-original]').getAttribute('data-original')
+                                //const imageUrlshort = longImgUrl&& longImgUrl.substring(longImgUrl.indexOf('https://www.quzu.com.tr/') + 24)
+                                const title = productCard.querySelector('.detailLink').getAttribute('title')
+
+                                return {
+                                    title: 'saygigiyim ' + title.replace(/İ/g, 'i').toLowerCase(),
+                                    priceNew,//:priceNew.replace('.','').replace(',','.').trim(),
+                                    imageUrl: longImgUrl,
+                                    link,
+                                    timestamp: Date.now(),
+                                    marka: 'saygigiyim',
+                                    //  subcategory: _subcategory,
+                                    category: _category,
+                                    node: _node
+                                }
+                            }).filter(f => f.imageUrl !== null)
+                        }, subcategory, category, node)
+
+                        debugger
+                        console.log('data length_____', data.length, 'url:', url)
+                        debugger
+                        const withSub = data.map(m => {
+                            const { title } = m
+                            const subcatmatches = subcategory.filter(f => title.toLowerCase().includes(f))
+                            const subcat = subcatmatches.length > 0 ? subcatmatches[0] : subcategory[subcategory.length - 1]
+                            debugger
+                            return { ...m, subcategory: subcat }
+                        })
+
+                        return resolve(withSub)
+
+                    }
+
+                }, 170)
+                // clearInterval(inv)
+            } catch (error) {
+                debugger
+                return reject(error)
+            }
+        })
+    } else {
+        console.log('no product')
+        return []
+    }
+
+
+
 }
 
 
