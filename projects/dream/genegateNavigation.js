07081ec2d1b2e846b5da417015b7b07d1380bc1c
webapis <60083568+webapis@users.noreply.github.com>
2022-09-15 14:16:42 +0500

    Update genegateNavigation.js

--- projects/dream/genegateNavigation.js
+++ projects/dream/genegateNavigation.js
@@ -8,25 +8,20 @@
   await genNav({ node: 'dream', subcategory: 'elbise' })
   await genNav({ node: 'dream', subcategory: 'pantolon' })
   await genNav({ node: 'dream', subcategory: 'etek' })
-  await genNav({ node: 'dream', subcategory: 'tulum' })
   await genNav({ node: 'dream', subcategory: 'gömlek' })
   await genNav({ node: 'dream', subcategory: 'tişört' })
   await genNav({ node: 'dream', subcategory: 'sweatshirt' })
-  await genNav({ node: 'dream', subcategory: 'hırka' })
+
 
 
   await genNav({ node: 'dream', subcategory: 'bluz' })
   await genNav({ node: 'dream', subcategory: 'şort' })
-  await genNav({ node: 'dream', subcategory: 'ceket' })
+
   await genNav({ node: 'dream', subcategory: 'tunik' })
   await genNav({ node: 'dream', subcategory: 'tayt' })
-  await genNav({ node: 'dream', subcategory: 'yağmurluk' })
-  await genNav({ node: 'dream', subcategory: 'mont' })
-  await genNav({ node: 'dream', subcategory: 'trençkot' })
 
-  await genNav({ node: 'dream', subcategory: 'kaban' })
+
   await genNav({ node: 'dream', subcategory: 'atlet' })
-  await genNav({ node: 'dream', subcategory: 'abiye' })
   await genNav({ node: 'dream', subcategory: 'dis_giyim' })
   await genNav({ node: 'dream', subcategory: 'kazak' })
   process.exit(0)
