0f7a27c4f0d29af79c617529f939b8e9300faaa4
webapis <60083568+webapis@users.noreply.github.com>
2022-09-16 11:57:29 +0500

    c

--- projects/dream/netlify/functions/gomlek-navsecond.js
+++ projects/dream/netlify/functions/gomlek-navsecond.js
@@ -4,6 +4,6 @@ const { netlifyNavHandler } = require('../../netlifyNavHandler')
 exports.handler = async function (event, context) {
 
 
-return await  netlifyNavHandler({ event, subcategory: 'gömlek', keyOrder: '1' })
+return await  netlifyNavHandler({ event, subcategory: 'gomlek', keyOrder: '1' })
 }
 
