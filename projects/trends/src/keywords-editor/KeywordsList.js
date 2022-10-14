434a673015ad6411c42d9beca9c2646429f9bd5a
webapis <60083568+webapis@users.noreply.github.com>
2022-10-14 13:44:58 +0500

    X

--- projects/trends/src/keywords-editor/KeywordsList.js
+++ projects/trends/src/keywords-editor/KeywordsList.js
@@ -52,9 +52,19 @@ useEffect(()=>{
         dispatch(actions.setAddKeywords())
     }
 
+    async function publishKeywords(){
+        const response = await fetch('/.netlify/functions/publishKeywords', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+
+        });
+        debugger
+    }
     return <Container> <Grid container style={{ marginTop: 100 }} gap={2}>
         <Grid item xs={12}>
-            {!addKeywords && <Button onClick={handleAddKeywords}>Add Keywords</Button>}
+            {!addKeywords && <div><Button onClick={handleAddKeywords}>Add Keywords</Button><Button onClick={publishKeywords}>Publish</Button></div>  }
             {!addKeywords && <Grid item xs={12} style={{ display: 'flex', justifyContent: 'flex-end' }}><KeywordsFilter /></Grid>}
         </Grid>
 
@@ -83,3 +93,28 @@ function groupBy(xs, key) {
         return rv;
     }, {});
 };
+
+
+
+async function triggerAction({ ticket, body, gh_action_url }) {
+
+
+    try {
+        const response = await fetch(gh_action_url, {
+            method: 'post',
+            headers: {
+                authorization: `token ${ticket}`,
+                Accept: 'application/vnd.github.v3+json'
+            },
+            body
+        })
+        const data = await response.json()
+    } catch (error) {
+
+    }
+
+}
+
+//const body = JSON.stringify({ ref: 'main', inputs: { projectName: workspaceName, parameters } })
+
+//const response = await triggerAction({ gh_action_url: `https://api.github.com/repos/webapis/coll2030/actions/workflows/generateKeywords.yml/dispatches`, ticket: token, body })
