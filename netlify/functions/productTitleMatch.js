62d3dd11979e00f58d21827effc6eb7b0722f23d
webapis <60083568+webapis@users.noreply.github.com>
2022-11-14 17:06:27 +0500

    c

--- netlify/functions/productTitleMatch.js
+++ netlify/functions/productTitleMatch.js
@@ -1,43 +1,53 @@
 
 function productTitleMatch({ kw, title, nws }) {
 
-    if(title){
+    if (title) {
 
-     
 
-    const match = kw.split(',').some(function (keyword) {
-     // let  regex ='(^|\\s)'+keyword+'(\\s|\\b)'.replace(' ','')
-        if (nws.length > 0) {
-            if (nws.filter((f) => title.toLowerCase().indexOf(f.toLowerCase()) !== -1).length > 0) {
-                return false
+
+        const match = kw.split(',').some(function (keyword) {
+            // let  regex ='(^|\\s)'+keyword+'(\\s|\\b)'.replace(' ','')
+            if (nws.length > 0) {
+                if (nws.filter((f) => {
+                    if (f.toLowerCase() === 'şal detaylı') {
+                        debugger
+                    }
+                    const exists = title.toLowerCase().indexOf(f.toLowerCase()) !== -1
+                    if (f.toLowerCase() === 'şal detaylı') {
+                        debugger
+                    }
+                    return exists
+                }).length > 0) {
+
+                    return false
+                } else {
+
+                    // if (exactmatch) {
+
+                    return title.toLowerCase().split(' ').find(f => f === keyword.toLowerCase())//.replace(/\s/g, ',').split(',').filter(f => f === keyword).length > 0
+                    // } else {
+
+                    //   return title.toLowerCase().replace(/\s/g, ',').split(',').filter(f => f === keyword || f.indexOf(keyword) === 0).length > 0
+                    //  }
+                }
+
             } else {
+                // if (exactmatch) {
+                return title.toLowerCase().split(' ').find(f => f === keyword.toLowerCase())//.replace(/\s/g, ',').split(',').filter(f => f === keyword).length > 0
+                //}// else {
+                //  return title.toLowerCase().replace(/\s/g, ',').split(',').filter(f => f === keyword || f.indexOf(keyword) === 0).length > 0
+                //  }
 
-               // if (exactmatch) {
-         
-                    return title.toLowerCase().split(' ').find(f=>f===keyword.toLowerCase())//.replace(/\s/g, ',').split(',').filter(f => f === keyword).length > 0
-               // } else {
-
-                 //   return title.toLowerCase().replace(/\s/g, ',').split(',').filter(f => f === keyword || f.indexOf(keyword) === 0).length > 0
-              //  }
             }
 
-        } else {
-           // if (exactmatch) {
-                return title.toLowerCase().split(' ').find(f=>f===keyword.toLowerCase())//.replace(/\s/g, ',').split(',').filter(f => f === keyword).length > 0
-            //}// else {
-              //  return title.toLowerCase().replace(/\s/g, ',').split(',').filter(f => f === keyword || f.indexOf(keyword) === 0).length > 0
-          //  }
 
-        }
+        })
 
+        return match
+    } else {
 
-    })
-
-    return match
-} else{
-    
-    return false
-}
+        return false
+    }
 }
 
 
