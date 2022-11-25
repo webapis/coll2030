8d49b2f3f927dd493e3c64485cc27b3558f12a64
webapis <60083568+webapis@users.noreply.github.com>
2022-11-25 12:39:36 +0500

    Update HorizontalNav.js

--- src/drawer/HorizontalNav.js
+++ src/drawer/HorizontalNav.js
@@ -6,6 +6,8 @@ import Fab from '@mui/material/Fab';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import React from 'react';
+
+const dataURL ="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAJYAAADIBAMAAAD4qwVWAAAAG1BMVEXMzMyWlpacnJyqqqqjo6O3t7fFxcWxsbG+vr6ayVztAAAACXBIWXMAAA7EAAAOxAGVKw4bAAABKUlEQVRoge3SMU/CQByG8beFtitCBcc71OgIie5cPwG4uNKgO0ys8M29nkJijMPVmjA8v6H5t0mfXK8nAQAAAAAAAAAAAMA/s9/uEpOYy2zl7VM/Wn9I+VZvOpMK+yBtytqkg3Ts7qU3P8e33LSUdrd2K1vNm9bw2R5yW7n4VjbSfCm7rhfZRK5pjXRcFM0c3eqPVS+0V238tGpaN0qa+RjfSgf+VeWvfklGYb+aB2GObvlQOsjdcG78lJxaYW63rv5kHdZybvlLi3WF/UrM536dv7HdfoX/mI61M9m1v3y1/FMX3wrnqxg+VRPZvT215CrbohXO/Uv5XmozPZ5bPT/Htn7nT103iketZh21slFuDx21ZK/KrlLa3G07awEAAAAAAAAAAFyYD/jJJjwPERThAAAAAElFTkSuQmCC"
 function HorizontalNav({navitems,selectSubcategory}) {
   const [items] = React.useState(navitems);
   const [selected, setSelected] = React.useState([]);
@@ -70,13 +72,12 @@ function RightArrow() {
 
 
 
-function Card({ onClick, title,m,selectSubcategory }) {
+function Card({ onClick, title,m,selectSubcategory,itemId }) {
   const visibility = React.useContext(VisibilityContext);
   const imageElement = useRef(null);
   useEffect(() => {
 
-
-      if (window.IntersectionObserver) {
+      if (window.IntersectionObserver && visibility.isItemVisible(itemId)) {
 
           let observer = new IntersectionObserver((entries, observer) => {
               entries.forEach(entry => {
@@ -94,7 +95,8 @@ function Card({ onClick, title,m,selectSubcategory }) {
 
 
 
-  }, []);
+  }, [visibility,itemId]);
+  
   return (
     <div
       onClick={() => onClick(visibility)}
@@ -102,7 +104,8 @@ function Card({ onClick, title,m,selectSubcategory }) {
       tabIndex={0}
     >
       <div  style={{ display: 'flex', flexDirection:'column' }}>
-                <img alt={title}  ref={imageElement} src={`https://via.placeholder.com/150x200?Text=${title}`}  height={200} data-src={`https://res.cloudinary.com/codergihub/image/upload/h_300/categories/${title}.jpg`} onClick={() => selectSubcategory({ functionName: m.functionName, index: m.index, groupName: m.groupName, keywordType: m.keywordType ? m.keywordType : 'category' })} style={{ borderRadius: 6, marginRight: 4 }}  />
+ 
+                <img alt={title}  ref={imageElement} src={dataURL}  height={200} data-src={`https://res.cloudinary.com/codergihub/image/upload/h_300/categories/${title}.jpg`} onClick={() => selectSubcategory({ functionName: m.functionName, index: m.index, groupName: m.groupName, keywordType: m.keywordType ? m.keywordType : 'category' })} style={{ borderRadius: 6, marginRight: 4 }}  />
                 <span style={{ fontSize: 14,opacity:0.7 ,textAlign:'center',padding:3}}>{title}</span></div>
 
     </div>
