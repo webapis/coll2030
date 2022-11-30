e0ccefd11ff315e8c5f749d51740e85e463a8000
webapis <60083568+webapis@users.noreply.github.com>
2022-11-30 11:06:41 +0500

    cc

--- src/App.js
+++ src/App.js
@@ -422,7 +422,7 @@ export default class App extends React.Component {
       <Helmet>
         <meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-      
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description"
           content={ new Date().toLocaleDateString()+" Moda markaları tek yerde ara. İstediğin giyim ürünü hızlı ve anında bul. Fiyat karşılaştır. Markadan satın al."} />
         <title>Tüm Marka Giyimler bir arada - BİRARADAMODA</title>
