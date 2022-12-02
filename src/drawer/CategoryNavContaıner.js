4ef29a639eccd07954ae032bffe7c4881b4715ba
webapis <60083568+webapis@users.noreply.github.com>
2022-12-02 15:03:50 +0500

    cc

--- src/drawer/CategoryNavContaıner.js
+++ src/drawer/CategoryNavContaıner.js
@@ -11,7 +11,8 @@ export default function CategoryNavContainer({ subcategories, products, fetching
   return products.length === 0 && !fetchingProduct && <Grid container sx={{ marginTop: 5 }}>
     {subcategories.filter((f)=>f[0]!=='diger').map((g)=>{
       const groupName = g[0]
-      const images = g[1]
+      const images = g[1].filter(f=> f.count !==undefined)
+      debugger
       let totalGroup=images.reduce((prev,curr)=>{
         return prev+curr.count
       },0)
