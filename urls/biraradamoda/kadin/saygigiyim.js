f6e75329e557741d78d6cc7864526b9d042084b5
webapis <60083568+webapis@users.noreply.github.com>
2023-01-19 09:15:26 +0500

    sementa

--- urls/biraradamoda/kadin/saygigiyim.js
+++ urls/biraradamoda/kadin/saygigiyim.js
@@ -1,21 +1,21 @@
 const urls = [
     { url: 'https://www.saygigiyim.com/elbise-141?currency=try',subcategory: ['elbise'], category: 'üst-giyim',node:'dream' },
-    // { url: 'https://www.bysaygi.com/tulum-146?currency=try',    subcategory: ['elbise'], category: 'üst-giyim', node: 'dream' },
+    { url: 'https://www.bysaygi.com/tulum-146?currency=try',    subcategory: ['elbise'], category: 'üst-giyim', node: 'dream' },
 
-    // { url: 'https://www.bysaygi.com/etek-150?currency=try',     subcategory: ['elbise'], category: 'alt-giyim',node:'dream' },
-    // { url: 'https://www.bysaygi.com/pantolon-145/?currency=try',subcategory: ['elbise'], category: 'alt-giyim',node:'dream' },
-    // { url: 'https://www.bysaygi.com/tayt-143?currency=try',     subcategory: ['elbise'], category: 'alt-giyim', node: 'dream' },
+    { url: 'https://www.bysaygi.com/etek-150?currency=try',     subcategory: ['elbise'], category: 'alt-giyim',node:'dream' },
+    { url: 'https://www.bysaygi.com/pantolon-145/?currency=try',subcategory: ['elbise'], category: 'alt-giyim',node:'dream' },
+    { url: 'https://www.bysaygi.com/tayt-143?currency=try',     subcategory: ['elbise'], category: 'alt-giyim', node: 'dream' },
 
 
-    // { url: 'https://www.bysaygi.com/ceket-155?currency=try',    subcategory: ['elbise'], category: 'dış-giyim', node: 'dream' },
-    // { url: 'https://www.bysaygi.com/trenckot-162?currency=try', subcategory: ['elbise'], category: 'dış-giyim', node: 'dream' },
+    { url: 'https://www.bysaygi.com/ceket-155?currency=try',    subcategory: ['elbise'], category: 'dış-giyim', node: 'dream' },
+    { url: 'https://www.bysaygi.com/trenckot-162?currency=try', subcategory: ['elbise'], category: 'dış-giyim', node: 'dream' },
    
 
-    // { url: 'https://www.bysaygi.com/sweat-158?currency=try',    subcategory: ['elbise'], category: 'üst-giyim', node: 'dream' },
-    // { url: 'https://www.bysaygi.com/bluz-142?currency=try',     subcategory: ['elbise'], category: 'üst-giyim', node: 'dream' },
-    // { url: 'https://www.bysaygi.com/body-168?currency=try',     subcategory: ['elbise'], category: 'üst-giyim', node: 'dream' },
-    // { url: 'https://www.bysaygi.com/gomlek-147?currency=try',   subcategory: ['elbise'], category: 'üst-giyim', node: 'dream' },
-    // { url: 'https://www.bysaygi.com/kazak-159?currency=try',     subcategory: ['elbise'], category: 'alt-giyim', node: 'dream' },
+    { url: 'https://www.bysaygi.com/sweat-158?currency=try',    subcategory: ['elbise'], category: 'üst-giyim', node: 'dream' },
+    { url: 'https://www.bysaygi.com/bluz-142?currency=try',     subcategory: ['elbise'], category: 'üst-giyim', node: 'dream' },
+    { url: 'https://www.bysaygi.com/body-168?currency=try',     subcategory: ['elbise'], category: 'üst-giyim', node: 'dream' },
+    { url: 'https://www.bysaygi.com/gomlek-147?currency=try',   subcategory: ['elbise'], category: 'üst-giyim', node: 'dream' },
+    { url: 'https://www.bysaygi.com/kazak-159?currency=try',     subcategory: ['elbise'], category: 'alt-giyim', node: 'dream' },
 ]
 
 module.exports={urls}
