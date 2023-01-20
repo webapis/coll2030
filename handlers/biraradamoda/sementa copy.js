672262d6310074d3626895f48b35756f50a58468
webapis <60083568+webapis@users.noreply.github.com>
2023-01-20 09:00:48 +0500

    trrr

--- handlers/biraradamoda/sementa copy.js
+++ handlers/biraradamoda/sementa copy.js
@@ -1,13 +1,50 @@
 const Apify = require('apify');
 const { formatMoney } = require('accounting-js')
 async function handler(page, context) {
-    const { request: { userData: { start } } } = context
+    const { request: { userData: { start} } } = context
     debugger;
     const url = await page.url()
 
     await page.waitForSelector('#katalog')
 
-    await autoScroll(page)
+    const dataset = await Apify.openDataset();
+    const requestQueue = await Apify.openRequestQueue();
+    const { items } = await dataset.getData()
+    console.log('items',items)
+    debugger
+    // const products = items.filter(f=> f['PRODUCTS'] !==undefined).map(m=>m.PRODUCTS).flat().map(m=>{
+
+    //     return {title:'sementa '+m.TITLE+ " _" + process.env.GENDER,priceNew:formatMoney(parseFloat(m.PRICE_SELL), { symbol: "", precision: 2, thousand: ".", decimal: "," }),imageUrl:m.IMAGE.BIG,link:m.URL,timestamp: Date.now(),marka:'sementa'}
+    // })
+    // await dataset.drop()
+//     debugger
+if(start){
+
+
+    const {COUNT:productCount} =items[0]['CATEGORIES'][0]
+
+debugger
+    const totalPages = Math.ceil(productCount / 32)
+    const pageUrls = []
+
+    let pagesLeft = totalPages
+    for (let i = 1; i <= totalPages; i++) {
+
+        pageUrls.push(`${url}?pg=` + i)
+        --pagesLeft
+
+
+    }
+    debugger
+    for (let url of pageUrls) {
+
+
+        await requestQueue.addRequest({ url, userData: { start: false } })
+
+    }
+
+    await dataset.drop()
+}
 
     const data = await page.$$eval('.productItem', (productCards) => {
         return productCards.map(productCard => {
@@ -32,7 +69,7 @@ async function handler(page, context) {
 
     return data.map(m => { return { ...m, title: m.title + " _" + process.env.GENDER } })
 
-
+  
 }
 
 
@@ -42,30 +79,22 @@ async function handler(page, context) {
 async function autoScroll(page) {
     await page.evaluate(async () => {
 
-        let bottom = 0
+
         await new Promise((resolve, reject) => {
             var totalHeight = 0;
             var distance = 100;
             let inc = 0
             var timer = setInterval(() => {
                 var scrollHeight = document.body.scrollHeight;
-               
+                var toth = 7775
                 window.scrollBy(0, distance);
                 totalHeight += distance;
                 inc = inc + 1
                 if (totalHeight >= scrollHeight - window.innerHeight) {
-                    if (bottom === 200) {
-                        clearInterval(timer);
-                        resolve();
-                    }
-                    else {
-                        bottom = bottom + 1
-                    }
-
-                }else{
-                    bottom=0  
+                    clearInterval(timer);
+                    resolve();
                 }
-            }, 50);
+            }, 200);
         });
     });
 }
--- urls/biraradamoda/kadin/tozlu.js
+++ urls/biraradamoda/kadin/tozlu.js
@@ -1,14 +1,14 @@
 const urls = [
   
-    // { url: 'https://www.tozlu.com/TR/kadin-giyim',              subcategory: ['elbise'], category: 'üst-giyim',node:'dream' },
-    // { url: 'https://www.tozlu.com/TR/kadin-dis-giyim', subcategory: ['elbise'], category: 'üst-giyim', node: 'dream' },
-    // { url: 'https://www.tozlu.com/TR/kadin-ic-giyim', subcategory: ['elbise'], category: 'üst-giyim', node: 'dream' },
-    // { url: 'https://www.tozlu.com/TR/plaj-modasi', subcategory: ['elbise'], category: 'üst-giyim', node: 'dream' },
+     { url: 'https://www.tozlu.com/TR/kadin-giyim' },
+     { url: 'https://www.tozlu.com/TR/kadin-dis-giyim' },
+     { url: 'https://www.tozlu.com/TR/kadin-ic-giyim' },
+     { url: 'https://www.tozlu.com/TR/plaj-modasi' },
 
-     { url: 'https://www.tozlu.com/TR/kadin-ayakkabi', subcategory: ['aksesuar'], category: 'üst-giyim', node: 'dream' },
+     { url: 'https://www.tozlu.com/TR/kadin-ayakkabi'},
 
-    // { url: 'https://www.tozlu.com/TR/kadin-canta', subcategory: ['aksesuar'], category: 'üst-giyim', node: 'dream' },
-    // { url: 'https://www.tozlu.com/TR/kadin-aksesuar', subcategory: ['aksesuar'], category: 'üst-giyim', node: 'dream' },
+     { url: 'https://www.tozlu.com/TR/kadin-canta' },
+     { url: 'https://www.tozlu.com/TR/kadin-aksesuar' }
 
 ]
 
