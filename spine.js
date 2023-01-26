42f1584595d477cbefa508b3e93c595025c7864e
webapis <60083568+webapis@users.noreply.github.com>
2023-01-26 09:31:42 +0500

    init

--- spine.js
+++ spine.js
@@ -1,11 +1,51 @@
 
-(async()=>{
-    const Apify = require('apify');
 
-    const productsDataset = await Apify.openDataset(`products`);
-    const { items: productItems } = await productsDataset.getData();
-    
-productItems.forEach(f=>{
-    console.log('f',f)
+const fs = require('fs')
+
+var AdmZip = require("adm-zip");
+const {compress}=require('compressed-json')
+const path =require('path')
+
+const { walkSync } = require('./utils/walkSync')
+//const zip = new AdmZip(`${process.cwd()}/output.zip`);
+// Convert json object
+let markaProducts = []
+walkSync(path.join(process.cwd(), `data/biraradamoda/kadin/beymen`), async (filepath) => {
+    const data = JSON.parse(fs.readFileSync(filepath))
+    markaProducts.push(data)
 })
-})()
+// debugger
+var content = compress( markaProducts);
+// zip.addFile('comp.json',Buffer.from(JSON.stringify(content)))
+debugger
+//fs.writeFileSync('output.json',JSON.stringify(content));
+// //  zip.writeZip(`${process.cwd()}/compresses.zip`,JSON.stringify(content));
+
+ debugger
+
+
+
+function compressesFile(content, zipName,output){
+ const zip = new AdmZip();
+ zip.addFile(zipName,Buffer.from(JSON.stringify(content)))
+// zip.writeZip(output);
+ fs.writeFileSync(output, zip.toBuffer());
+}
+
+function decompressFile(filepath,zipName){
+
+    const zip = new AdmZip(filepath);
+    zip.extractEntryTo(zipName,'./');
+}
+
+decompressFile('downloaded.zip','beymen.json')
+//ompressesFile(content,'beymen.json','./beymen.zip')
+// decompressFile('./beymen.zip','beymen.json')
+// var zlib = require('zlib');
+
+
+// var gzip = zlib.createGzip();
+// var r = fs.createReadStream('output.json');
+// var w = fs.createWriteStream('output.json.gz');
+// r.pipe(gzip).pipe(w);
+
--- spine2.js
+++ spine2.js
@@ -1,10 +1,65 @@
 
-(async()=>{
-    const sharp = require('sharp');
-    const fs =require('fs')
-    const buffer =fs.readFileSync('./public/category-sprite/sprite.jpg')
-    await sharp(buffer)
-    .jpeg()
-    .toFile('./public/category-sprite/sprite-compressed.jpg');
+const fetch = require('node-fetch')
+const fs = require('fs')
+const {downloadCollection} =require('./utils/uploadCollection')
+async function pushContentToProjectBranch() {
 
-})()
+    let buff = fs.readFileSync('output.json');
+    let base64data = buff.toString('base64');
+
+    const response = await fetch(`https://api.github.com/repos/webapis/keyword-editor/contents/output.json`, { method: 'put', headers: { Accept: "application/vnd.github.v3+json", authorization: `token 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`, "X-GitHub-Api-Version": "2022-11-28" }, body: JSON.stringify({ message: 'coder content', content: base64data, branch: 'main' }) })
+    const data = await response.text()
+    console.log('response', response)
+
+
+}
+
+//pushContentToProjectBranch()
+
+
+async function fetchFile() {
+
+    const response = await fetch('https://api.github.com/repos/webapis/keyword-editor/contents/beymen.zip', { method: 'get', headers: { Accept: "application/vnd.github.raw", authorization: `token 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`, "X-GitHub-Api-Version": "2022-11-28" } })
+    debugger
+    var file = fs.createWriteStream("downloaded2.zip");
+
+    //     const data = await response.json()
+
+    //     const content = data.content
+    // debugger
+    //     let buff = new Buffer(content, 'base64');
+
+    //     let text = buff.toString('utf-8');
+    response.body.on('close',()=>{
+        console.log('closed')
+    })
+    response.body.pipe(file)
+
+}
+
+//fetchFile()
+
+
+async function getPluginsSourceCodeTree() {
+    // Retrieve source code for project
+    //Retrieved source code will be copied to project branch of forked agregators repo
+    //---- List branches endpoint----
+    /*required for the next endoint*/
+    const response = await fetch(`https://api.github.com/repos/webapis/keyword-editor/branches`,{ method: 'get', headers: { Accept: "application/vnd.github.raw", authorization: `token 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`, "X-GitHub-Api-Version": "2022-11-28" }})
+    const data = await response.json()
+    const mainSha = data.find(d => d.name === 'main')
+    const { commit: { sha } } = mainSha
+debugger
+    //------Git database / Get a tree endpoint------
+    /*required to retrieve list of file and folder into*/
+    const treeResponse = await fetch(`https://api.github.com/repos/webapis/keyword-editor/git/trees/${sha}?recursive=1`,{ method: 'get', headers: { Accept: "application/vnd.github.raw", authorization: `token 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`, "X-GitHub-Api-Version": "2022-11-28" }})
+    const treeData = await treeResponse.json()
+    const { tree } = treeData
+    debugger;
+    const dataFolderTrees =tree.filter(f=>f.type==='blob' && f.path.includes('data/'))
+    debugger
+    return dataFolderTrees
+}
+
+//getPluginsSourceCodeTree()
+downloadCollection('kadin')
