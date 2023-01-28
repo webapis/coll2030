be9e68405e65652b6c4a9dd63f136bf2ed63abd7
webapis <60083568+webapis@users.noreply.github.com>
2023-01-28 12:42:32 +0500

    penti

--- spine2.js
+++ spine2.js
@@ -1,19 +1,14 @@
 
-const fetch = require('node-fetch')
+
+(async ()=>{
+    const fetch = require('node-fetch')
 const fs = require('fs')
-const { getSingleContent, mergePrevAndNewData } = require('./utils/uploadCollection')
+const { getSingleContent, unzipSingleContent } = require('./utils/uploadCollection')
 
+   await getSingleContent(`kadin/sementa.json.gz`)
+  await  unzipSingleContent(`single-content/kadin/penti.json.gz`) 
+})
 
-//getSingleContent(`kadin/sementa.json.gz`)
-//unzipSingleContent(`single-content/kadin/sementa.json.gz`)
 debugger
 
-const data = [{
-    "title": "sementa kadın fermuarlı cepli triko ceket - siyah - gri _kadin",
-    "priceNew": "2.199,99",
-    "imageUrl": "kadin-fermuarli-cepli-triko-ceket-siyah-gri-ceket-sementa-18692-84-B.jpg",
-    "link": "kadin-fermuarli-cepli-triko-ceket-siyah-gri",
-    "timestamp": 1674646327660,
-    "marka": "sementa"
-}]
-mergePrevAndNewData({ gender: 'kadin', marka: 'sementa', data })
+
