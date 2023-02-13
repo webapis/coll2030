c33d3e1f1a8e4515d999e216068a124079ef7a8e
webapis <60083568+webapis@users.noreply.github.com>
2023-02-13 12:01:39 +0500

    vakko

--- urls/biraradamoda/kadin/vakko.js
+++ urls/biraradamoda/kadin/vakko.js
@@ -1,12 +1,12 @@
 const urls = [
 
   
-    // { url: 'https://www.vakko.com/kadin/giyim', subcategory: ['elbise'], category: 'üst-giyim', opts:[],node:'dream'},
+    { url: 'https://www.vakko.com/kadin/giyim'},
 
-    // { url: 'https://www.vakko.com/ayakkabi-canta/kadin-ayakkabi/', subcategory: ['aksesuar'], category: 'üst-giyim', opts:[],node:'dream'},
+    { url: 'https://www.vakko.com/ayakkabi-canta/kadin-ayakkabi/'},
     
-    // { url: 'https://www.vakko.com/kadin/esarp-sal-koleksiyonu', subcategory: ['aksesuar'], category: 'üst-giyim', opts:[],node:'dream'},
-    { url: 'https://www.vakko.com/kadin/aksesuar', subcategory: ['aksesuar'], category: 'üst-giyim', opts:[],node:'dream'},
+    { url: 'https://www.vakko.com/kadin/esarp-sal-koleksiyonu'},
+    { url: 'https://www.vakko.com/kadin/aksesuar'},
 ]
 
 module.exports = { urls }
