ebf326d6dd0978c3078c01eaa044321746a2251a
webapis <60083568+webapis@users.noreply.github.com>
2023-02-14 12:01:31 +0500

    xint

--- urls/biraradamoda/kadin/xint.js
+++ urls/biraradamoda/kadin/xint.js
@@ -1,27 +1,17 @@
 const urls = [
   
-    { url: 'https://www.xint.com.tr/kadin-elbise', subcategory: ['elbise'], category: 'üst-giyim' ,node:'dream'},
-
-
-
-    { url: 'https://www.xint.com.tr/kadin-etek', subcategory: ['elbise'], category: 'alt-giyim' ,node:'dream'},
-    { url: 'https://www.xint.com.tr/kadin-pantolon', subcategory: ['elbise'], category: 'alt-giyim' ,node:'dream'},
-  
-
-    { url: 'https://www.xint.com.tr/kadin-ceket', subcategory: ['elbise'], category: 'dış-giyim', node: 'dream' },
-    { url: 'https://www.xint.com.tr/kadin-mont', subcategory: ['elbise'], category: 'dış-giyim', node: 'dream' },
-    { url: 'https://www.xint.com.tr/kadin-yelek', subcategory: ['elbise'], category: 'dış-giyim', node: 'dream' },
-
-
-
-    { url: 'https://www.xint.com.tr/kadin-sweatshirt', subcategory: ['elbise'], category: 'üst-giyim', node: 'dream' },
-
-    { url: 'https://www.xint.com.tr/kadin-bluz', subcategory: ['elbise'], category: 'üst-giyim', node: 'dream' },
-    { url: 'https://www.xint.com.tr/kadin-tisort', subcategory: ['elbise'], category: 'üst-giyim', node: 'dream' },
-    { url: 'https://www.xint.com.tr/kadin-gomlek', subcategory: ['elbise'], category: 'üst-giyim', node: 'dream' },
-    { url: 'https://www.xint.com.tr/kadin-kazak', subcategory: ['elbise'], category: 'üst-giyim', node: 'dream' },
-
-    { url: 'https://www.xint.com.tr/kadin-ayakkabi', subcategory: ['ayakkabi'], category: 'üst-giyim', node: 'dream' },
+    { url: 'https://www.xint.com.tr/kadin-elbise'},
+    { url: 'https://www.xint.com.tr/kadin-etek'},
+    { url: 'https://www.xint.com.tr/kadin-pantolon'},
+    { url: 'https://www.xint.com.tr/kadin-ceket' },
+    { url: 'https://www.xint.com.tr/kadin-mont'},
+    { url: 'https://www.xint.com.tr/kadin-yelek' },
+    { url: 'https://www.xint.com.tr/kadin-sweatshirt' },
+    { url: 'https://www.xint.com.tr/kadin-bluz' },
+    { url: 'https://www.xint.com.tr/kadin-tisort' },
+    { url: 'https://www.xint.com.tr/kadin-gomlek' },
+    { url: 'https://www.xint.com.tr/kadin-kazak' },
+    { url: 'https://www.xint.com.tr/kadin-ayakkabi' },
 
 ]
 
