92cc537bbfa352dcec9d3da0069887d95ade20ce
webapis <60083568+webapis@users.noreply.github.com>
2023-02-15 12:11:09 +0500

    machka

--- handlers/biraradamoda/machka.js
+++ handlers/biraradamoda/machka.js
@@ -1,75 +1,83 @@
-
 async function handler(page, context) {
-    const { request: { userData: { } } } = context
-    debugger;
+    const { request: { userData: {  } } } = context
+
     const url = await page.url()
 
-    await page.waitForSelector('.ems-prd-list-page');
-debugger
-    await autoScroll(page)
+        return new Promise((resolve, reject) => {
+            try {
+                let totalProducts = 0
+                let collected = 0
+                let inv = setInterval(async () => {
 
-    const data = await page.$$eval('.ems-prd', (items, _subcategory, _category,_node) => {
+                    console.log('collected', collected,totalProducts)
 
-        return items.map(item => {
-            const priceNew = item.querySelector('.ems-prd-price-last') && item.querySelector('.ems-prd-price-last').innerText.replace('₺', '').trim()
+                    if (totalProducts>0 && totalProducts === collected) {
+                              clearInterval(inv)
 
-            const longlink = item.querySelector('.ems-prd-link.btn-full').href
-            const link = longlink.substring(longlink.indexOf('https://www.machka.com.tr/urun/') + 31)
-            const longImgUrl = item.querySelector('.ems-responsive-item').getAttribute('data-image-src')
-            const imageUrlshort = longImgUrl.substring(longImgUrl.indexOf('https://machka.mncdn.com/mnresize/660/-//Machka/products/') + 57)
+                              const data = await page.$$eval('.ems-prd', (items) => {
 
-            return {
-                title: 'machka ' + item.querySelector('.ems-prd-title').innerText.replace(/İ/g,'i').toLowerCase(),
-                priceNew,//: priceNew.replace('.', '').replace(',00', '').trim(),
-                imageUrl: imageUrlshort,
-                link,
-                timestamp: Date.now(),
-                marka: 'machka',
+                                return items.map(item => {
+                                    const priceNew = item.querySelector('.ems-prd-price-last') && item.querySelector('.ems-prd-price-last').innerText.replace('₺', '').trim()
+                        
+                                    const longlink = item.querySelector('.ems-prd-link.btn-full').href
+                                    const link = longlink.substring(longlink.indexOf('https://www.machka.com.tr/urun/') + 31)
+                                    const longImgUrl = item.querySelector('.ems-responsive-item').getAttribute('data-image-src')
+                                    const imageUrlshort = longImgUrl.substring(longImgUrl.indexOf('https://machka.mncdn.com/mnresize/660/-//Machka/products/') + 57)
+                        
+                                    return {
+                                        title: 'machka ' + item.querySelector('.ems-prd-title').innerText.replace(/İ/g,'i').toLowerCase(),
+                                        priceNew,//: priceNew.replace('.', '').replace(',00', '').trim(),
+                                        imageUrl: imageUrlshort,
+                                        link,
+                                        timestamp: Date.now(),
+                                        marka: 'machka',
+                        
+                                    }
+                                })
+                            });
 
-            }
-        })
-    });
+                            debugger
+                            console.log('data length_____', data.length, 'url:', url)
+                            debugger
+
+                            const remap =data.map(m=>{return {...m,title:m.title+" _"+process.env.GENDER }})
+                            return resolve(remap)
+                        //  await page.click('.button.js-load-more')
 
 
-    debugger;
-    console.log('data length_____', data.length)
-return data.map(m => { return { ...m, title: m.title + " _" + process.env.GENDER } })
-
-}
-
-
-
-
-
-async function autoScroll(page) {
-    await page.evaluate(async () => {
-
-
-        await new Promise((resolve, reject) => {
-            var totalHeight = 0;
-            var distance = 100;
-            let inc = 0
-            var timer = setInterval(() => {
-                var scrollHeight = document.body.scrollHeight;
-                var toth = 7775
-                window.scrollBy(0, distance);
-                totalHeight += distance;
-
-                if (totalHeight >= scrollHeight - window.innerHeight) {
-                    if (inc === 200) {
-                        clearInterval(timer);
-                        resolve();
                     } else {
-                        inc = inc + 1
+
+                        await manualScroll(page)
+                        totalProducts = await page.evaluate(() =>parseInt(document.querySelector('.prd-qty').innerHTML.replace(/[^\d]/g,'')))
+                        collected = await page.evaluate(() => document.querySelectorAll('.ems-prd').length)
+
                     }
 
-                } else {
-                    inc = 0
-                }
-            }, 50);
-        });
+                }, 150)
+                // clearInterval(inv)
+            } catch (error) {
+                console.log('error------',error)
+                debugger
+                return reject(error)
+            }
+        })
+  
+
+
+
+}
+
+async function manualScroll(page) {
+    await page.evaluate(async () => {
+        var totalHeight = 0;
+        var distance = 200;
+        let inc = 0
+        window.scrollBy(0, distance);
+        totalHeight += distance;
+        inc = inc + 1
     });
 }
+
 async function getUrls(page) {
 
     const pageUrls = []
