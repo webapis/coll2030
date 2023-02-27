5a0925227da13ed66aa8db9eb240711b8386d3f4
webapis <60083568+webapis@users.noreply.github.com>
2023-02-27 13:25:20 +0500

    tergan

---
+++ urls/biraradamoda/erkek/tergan.js
@@ -0,0 +1,12 @@
+const urls = [
+
+    { url: 'https://www.tergan.com.tr/erkek-ayakkabi'},  
+    { url: 'https://www.tergan.com.tr/erkek-deri-mont'},  
+    { url: 'https://www.tergan.com.tr/erkek-bot'},  
+    { url: 'https://www.tergan.com.tr/erkek-canta'},  
+    { url: 'https://www.tergan.com.tr/erkek-aksesuar'},  
+
+
+]
+
+module.exports = { urls }
