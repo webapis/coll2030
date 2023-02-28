5a22595698a565681f0e39d5cde642a18540734d
webapis <60083568+webapis@users.noreply.github.com>
2023-02-28 12:14:28 +0500

    mastras desa

---
+++ urls/biraradamoda/kadin/matras.js
@@ -0,0 +1,13 @@
+const urls = [
+
+   { url: 'https://www.matras.com/canta-modelleri'},
+   { url: 'https://www.matras.com/ayakkabi-modelleri'},
+   { url: 'https://www.matras.com/cuzdan-modelleri'},
+   { url: 'https://www.matras.com/giyim'},
+   { url: 'https://www.matras.com/kemer-modelleri'},
+   { url: 'https://www.matras.com/aksesuar'},
+   { url: 'https://www.matras.com/seyahat'},
+
+]
+
+module.exports = { urls }
