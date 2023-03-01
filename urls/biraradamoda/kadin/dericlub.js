ce202296898de0310d4fffa47fea87e463cda716
webapis <60083568+webapis@users.noreply.github.com>
2023-03-01 13:48:33 +0500

    dericlub action

--- urls/biraradamoda/kadin/dericlub.js
+++ urls/biraradamoda/kadin/dericlub.js
@@ -1,6 +1,6 @@
 const urls = [
 
-   { url: 'https://www.dericlub.com.tr/kadin'},
+   { url: 'https://www.dericlub.com.tr/erkek'},
    { url: '  https://www.dericlub.com.tr/seyahat-valiz-bavul'},
  
 
