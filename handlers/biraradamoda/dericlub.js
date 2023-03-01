8bc87a6f44a3863326863559fdf3602a9519fce2
webapis <60083568+webapis@users.noreply.github.com>
2023-03-01 13:42:00 +0500

    Update dericlub.js

--- handlers/biraradamoda/dericlub.js
+++ handlers/biraradamoda/dericlub.js
@@ -1,61 +1,87 @@
 
-const { formatMoney } = require('accounting-js')
+const Apify = require('apify');
 async function handler(page, context) {
-    const { request: { userData: { } } } = context
+    const { request: { userData: { start, detailPage } } } = context
 
+    const requestQueue = await Apify.openRequestQueue();
     const url = await page.url()
-
-    await page.waitForSelector('#ProductPageProductList')
-    await page.waitForSelector('.ItemOrj')
     debugger
-    const data = await page.$$eval('.productItem', (productCards) => {
-        return productCards.map(productCard => {
+    if (start) {
+        debugger
+        await page.waitForSelector('#ProductPageProductList')
+        const totalPages = await page.evaluate(() => Math.max(...Array.from(document.querySelectorAll('.pageBorder a')).map(m => m.innerHTML.replace(/[^\d]/g, '')).filter(Number)))
+        // const totalPages = Math.ceil(productCount / 60)
+        const pageUrls = []
 
-            const imageUrl = productCard.querySelector(".productSliderImage").currentSrc ? productCard.querySelector(".productSliderImage").currentSrc: document.querySelector('.productItem').outerHTML
-            const title = productCard.querySelector('.detailLink.detailUrl') ? productCard.querySelector('.detailLink.detailUrl').getAttribute('title') : document.querySelector('.productItem').outerHTML
-            const priceNew = productCard.querySelector('.discountPrice span').innerText.replace('₺', '')
-            const longlink = productCard.querySelector('.detailLink.detailUrl').href
-            const link = longlink.substring(longlink.indexOf("https://www.dericlub.com.tr/") + 28)
+        let pagesLeft = totalPages
+        for (let i = 2; i <= totalPages; i++) {
 
-            const imageUrlshort = imageUrl && imageUrl.substring(imageUrl.indexOf("https://static.ticimax.cloud/") + 29)
+            //pageUrls.push(`${url}?sayfa=` + i)
+            //--pagesLeft
+            debugger
+            await requestQueue.addRequest({ url: `${url}?sayfa=` + i, userData: { start: false } })
+        }
 
-            return {
-                title: 'dericlub ' + title.replace(/İ/g, 'i').toLowerCase(),
-                priceNew,
-                imageUrl: imageUrlshort,
-                link,
+    }
+    if (detailPage) {
+
+        debugger
+        await page.waitForSelector('#ProductDetailMain')
+        debugger
+        const data = await page.evaluate(() => {
+            const renk = document.querySelector('.size_box.selected').innerHTML.replace('/', '-')
+            return [{
+                title: 'dericlub ' + document.querySelector('.ProductName span').textContent.replaceAll('\n', '').toLowerCase() + ' ' + renk,
+                priceNew: document.querySelector('.spanFiyat').textContent.replace('₺', ''),
+                imageUrl: document.querySelector('#imgUrunResim').src,
+                link: location.href,
                 timestamp: Date.now(),
                 marka: 'dericlub',
-            }
-        }).filter(f => f.imageUrl !== null && f.title.length > 3)
-    })
-
-    console.log('data length_____', data.length, 'url:', url, process.env.GENDER)
-    debugger
-    console.log("process.env.GENDER ")
-    const formatprice = data.map((m) => {
-        return { ...m, title: m.title + " _" + process.env.GENDER }
-    })
+            }]
+        })
+        debugger
+        console.log('data length_____', data.length, 'url:', url, process.env.GENDER)
+        return data.map((m) => { return { ...m, title: m.title + " _" + process.env.GENDER } })
+    } else {
 
 
-    return formatprice
+        await page.waitForSelector('#ProductPageProductList')
+        await page.waitForSelector('.ItemOrj')
+
+        const data = await page.$$eval('.productItem', (productCards) => {
+            return productCards.map(productCard => {
+                const longlink = productCard.querySelector('.detailLink.detailUrl').href
+                return {
+                    link: longlink,
+                }
+            }).filter(f => f.link !== null)
+        })
+
+        for (let url of data) {
+            debugger
+            await requestQueue.addRequest({ url: url.link, userData: { start: false, detailPage: true } })
+        }
+        return []
+    }
+
 }
 
 async function getUrls(page) {
-    const url = await page.url()
-    await page.waitForSelector('.appliedFilter.FiltrelemeUrunAdet span')
-    const totalPages = await page.evaluate(() => Math.max(...Array.from(document.querySelectorAll('.appliedFilter.FiltrelemeUrunAdet span')).map(m => m.innerHTML.replace(/[^\d]/g, '')).filter(Number)))
+
+    // const url = await page.url()
+    //  await page.waitForSelector('.appliedFilter.FiltrelemeUrunAdet span')
+    //  const totalPages = await page.evaluate(() => Math.max(...Array.from(document.querySelectorAll('.appliedFilter.FiltrelemeUrunAdet span')).map(m => m.innerHTML.replace(/[^\d]/g, '')).filter(Number)))
     // const totalPages = Math.ceil(productCount / 60)
     const pageUrls = []
 
-    let pagesLeft = totalPages
-    for (let i = 2; i <= totalPages; i++) {
+    // let pagesLeft = totalPages
+    // for (let i = 2; i <= totalPages; i++) {
 
-        pageUrls.push(`${url}?sayfa=` + i)
-        --pagesLeft
+    //     pageUrls.push(`${url}?sayfa=` + i)
+    //     --pagesLeft
 
 
-    }
+    // }
 
     return { pageUrls, productCount: 0, pageLength: pageUrls.length + 1 }
 }
