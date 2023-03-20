50a1dcf40e5a8ac1eac9c6c03f5767678907bd61
webapis <60083568+webapis@users.noreply.github.com>
2023-03-20 12:20:20 +0500

    wcollection

--- main.js
+++ main.js
@@ -193,6 +193,8 @@ Apify.main(async () => {
 
 
     if (productItems.length > 0) {
+        const groupByImageURL =groupBy(productItems,'imageUrl')
+        debugger
         console.log('productItems', productItems.length)
         debugger
         const uniqueProductCollection = uniqify(productItems, 'imageUrl')
