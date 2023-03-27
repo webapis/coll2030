d58a2016912aa90d23b5226ed71bdf209014c6bb
webapis <60083568+webapis@users.noreply.github.com>
2023-03-27 10:10:59 +0500

    Update machka.js

--- urls/biraradamoda/kadin/machka.js
+++ urls/biraradamoda/kadin/machka.js
@@ -1,34 +1,22 @@
 const urls = [
   //  { url: 'https://www.machka.com.tr/sort_691', subcategory: 'şort', category: 'alt-giyim' },
-    { url: 'https://www.machka.com.tr/elbise-modelleri-c_828'},
 
-
-    { url: 'https://www.machka.com.tr/etek-modelleri-c_829'},
-    { url: 'https://www.machka.com.tr/kadin-pantolon-c_832'},
-
-
-
-    { url: 'https://www.machka.com.tr/kadin-ceket-c_827' },
-    { url: 'https://www.machka.com.tr/kaban-c_830'},
-    { url: 'https://www.machka.com.tr/kadin-mont-c_831' },
-    { url: 'https://www.machka.com.tr/yelek-c_857' },
-    { url: 'https://www.machka.com.tr/pardosu-modelleri-c_833' },
-
-
-
-
-
-
-    { url: 'https://www.machka.com.tr/sweat-shirt',            },
-    { url: 'https://www.machka.com.tr/bluz-c_826',           },
-    { url: 'https://www.machka.com.tr/kadin-gomlek-c_853',    },
-    { url: 'https://www.machka.com.tr/tunik-modelleri-c_855',  },
-    { url: 'https://www.machka.com.tr/kap',                    },
-    { url: 'https://www.machka.com.tr/triko-modelleri-c_834',  },
-    { url: 'https://www.machka.com.tr/penye-modelleri-c_858', },
-    { url: 'https://www.machka.com.tr/triko-modelleri-c_834', },
-
-
+    { url: 'https://www.machka.com.tr/kadin-sort-modelleri'},
+    { url: 'https://www.machka.com.tr/kadin-elbise-modelleri'},
+    { url: 'https://www.machka.com.tr/kadin-bluz-modelleri'},
+    { url: 'https://www.machka.com.tr/kadin-gomlek-modelleri'},
+    { url: 'https://www.machka.com.tr/tunik-modelleri'},
+    { url: 'https://www.machka.com.tr/kadin-ceket-modelleri'},
+    { url: 'https://www.machka.com.tr/kadin-kap-modelleri'},
+    { url: 'https://www.machka.com.tr/etek-modelleri'},
+    { url: 'https://www.machka.com.tr/kadin-pantolon-modelleri'},
+    { url: 'https://www.machka.com.tr/kadin-triko-kazak-modelleri'},
+    { url: 'https://www.machka.com.tr/kadin-sweatshirt-modelleri'},
+    { url: 'https://www.machka.com.tr/kadin-penye-tisort-modelleri'},
+    { url: 'https://www.machka.com.tr/kadin-tulum-modelleri'},
+    { url: 'https://www.machka.com.tr/kadin-pardosu-modelleri'},
+    { url: 'https://www.machka.com.tr/kadin-kaban-modelleri'},
+    { url: 'https://www.machka.com.tr/kadin-mont-modelleri'},
 
   ]
 
