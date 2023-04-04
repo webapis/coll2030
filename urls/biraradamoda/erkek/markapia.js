102008b43e1c83f495b8f9dafc1572eb7a43029b
webapis <60083568+webapis@users.noreply.github.com>
2023-04-04 15:44:57 +0500

    Update markapia.js

--- urls/biraradamoda/erkek/markapia.js
+++ urls/biraradamoda/erkek/markapia.js
@@ -1,9 +1,9 @@
 const urls = [
 
     { url: 'https://www.markapia.com/erkek-giyim'},
-    // { url: 'https://www.markapia.com/erkek-spor-giyim'},
-    // { url: 'https://www.markapia.com/erkek-ic-giyim'},
-    // { url: 'https://www.markapia.com/erkek-aksesuar'},
+    { url: 'https://www.markapia.com/erkek-spor-giyim'},
+     { url: 'https://www.markapia.com/erkek-ic-giyim'},
+    { url: 'https://www.markapia.com/erkek-aksesuar'},
 
   
 
