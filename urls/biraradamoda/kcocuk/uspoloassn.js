c73e4947a3ccd056a03a50c2ba6e94ee043d1672
webapis <60083568+webapis@users.noreply.github.com>
2023-04-05 12:59:02 +0500

    polo

--- urls/biraradamoda/kcocuk/uspoloassn.js
+++ urls/biraradamoda/kcocuk/uspoloassn.js
@@ -1,6 +1,20 @@
 const urls = [
   
-    { url: 'https://tr.uspoloassn.com/cocuk-tum-urunler/?attributes_filterable_gender=K%C4%B1z%20%C3%87ocuk'},
+    { url: 'https://tr.uspoloassn.com/kiz-cocuk-ayakkabi-terlik'},
+    { url: 'https://tr.uspoloassn.com/kiz-cocuk-sweatshirt'},
+    { url: 'https://tr.uspoloassn.com/kiz-cocuk-triko'},
+    { url: 'https://tr.uspoloassn.com/kiz-cocuk-gomlek'},
+    { url: 'https://tr.uspoloassn.com/kiz-cocuk-esofman'},
+    { url: 'https://tr.uspoloassn.com/kiz-cocuk-pantolon'},
+    { url: 'https://tr.uspoloassn.com/kiz-cocuk-etek-elbise'},
+    { url: 'https://tr.uspoloassn.com/kiz-cocuk-kaban-ve-mont'},
+    { url: 'https://tr.uspoloassn.com/kiz-cocuk-yelek'},
+    { url: 'https://tr.uspoloassn.com/kiz-cocuk-kaskol-bere-eldiven'},
+    { url: 'https://tr.uspoloassn.com/kiz-cocuk-sort-capri'},
+    { url: 'https://tr.uspoloassn.com/kiz-cocuk-corap'},
+    { url: 'https://tr.uspoloassn.com/kiz-cocuk-okul-cantasi'},
+    { url: 'https://tr.uspoloassn.com/kiz-cocuk-polo-tshirt'},
+    { url: 'https://tr.uspoloassn.com/kiz-cocuk-pijama'},
 
 
 ]
