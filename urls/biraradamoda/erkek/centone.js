6b38a36fccf273ab887a83eca3a302f78ab4836e
webapis <60083568+webapis@users.noreply.github.com>
2023-04-06 10:22:29 +0500

    Update centone.js

--- urls/biraradamoda/erkek/centone.js
+++ urls/biraradamoda/erkek/centone.js
@@ -1,6 +1,6 @@
 const urls = [
-    { url: 'https://www.centone.com.tr/gomlek'},
-  //  { url: 'https://www.centone.com.tr/urunler'},
+ //   { url: 'https://www.centone.com.tr/gomlek'},
+  { url: 'https://www.centone.com.tr/urunler'},
   
 
 ]
