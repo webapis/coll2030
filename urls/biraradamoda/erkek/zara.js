1650ed5b236e664112bca1a9d4801d60e983238b
Serdar Ashirov <webapis@users.noreply.github.com>
2023-04-23 16:23:11 +0500

    zara men

--- urls/biraradamoda/erkek/zara.js
+++ urls/biraradamoda/erkek/zara.js
@@ -1,19 +1,19 @@
 const urls = [
   
-    { url: 'https://www.zara.com/tr/tr/erkek-gemlekler-l737.html?v1=2204881' },
-    { url: 'https://www.zara.com/tr/tr/erkek-ceket-yelek-l1650.html?v1=2206373' },
-    { url: 'https://www.zara.com/tr/tr/erkek-blazerlar-l608.html?v1=2204836' },
-    { url: 'https://www.zara.com/tr/tr/man-overshirts-l3174.html?v1=2204784' },
-    { url: 'https://www.zara.com/tr/tr/erkek-erge-giyim-l681.html?v1=2206375' },
-    { url: 'https://www.zara.com/tr/tr/erkek-sweatshirtler-l821.html?v1=2204863' },
-    { url: 'https://www.zara.com/tr/tr/erkek-pantolonlar-l838.html?v1=2206461' },
-    { url: 'https://www.zara.com/tr/tr/erkek-tishertler-l855.html?v1=2206614' },
-    { url: 'https://www.zara.com/tr/tr/erkek-polo-yakalar-l733.html?v1=2206446' },
-    { url: 'https://www.zara.com/tr/tr/erkek-takim-elbiseler-l808.html?v1=2204814' },
-    { url: 'https://www.zara.com/tr/tr/erkek-bermudalar-l592.html?v1=2204967' },
-    { url: 'https://www.zara.com/tr/tr/erkek-ayakkabilar-l769.html?v1=2206091' },
-    { url: 'https://www.zara.com/tr/tr/erkek-chantalar-l563.html?v1=2207124' },
-    { url: 'https://www.zara.com/tr/tr/erkek-aksesuarlar-l537.html?v1=2206721' },
+    { url: 'https://www.zara.com/tr/tr/erkek-gemlekler-l737.html' },
+    { url: 'https://www.zara.com/tr/tr/erkek-ceket-yelek-l1650.html' },
+    { url: 'https://www.zara.com/tr/tr/erkek-blazerlar-l608.html' },
+    { url: 'https://www.zara.com/tr/tr/man-overshirts-l3174.html' },
+    { url: 'https://www.zara.com/tr/tr/erkek-erge-giyim-l681.html' },
+    { url: 'https://www.zara.com/tr/tr/erkek-sweatshirtler-l821.html' },
+    { url: 'https://www.zara.com/tr/tr/erkek-pantolonlar-l838.html' },
+    { url: 'https://www.zara.com/tr/tr/erkek-tishertler-l855.html' },
+    { url: 'https://www.zara.com/tr/tr/erkek-polo-yakalar-l733.html' },
+    { url: 'https://www.zara.com/tr/tr/erkek-takim-elbiseler-l808.html' },
+    { url: 'https://www.zara.com/tr/tr/erkek-bermudalar-l592.html' },
+    { url: 'https://www.zara.com/tr/tr/erkek-ayakkabilar-l769.html' },
+    { url: 'https://www.zara.com/tr/tr/erkek-chantalar-l563.html' },
+    { url: 'https://www.zara.com/tr/tr/erkek-aksesuarlar-l537.html' },
 
 ]
 
