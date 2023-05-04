08b3bffed12c7ccd39ecd2c92d05acf1cab6b199
Serdar Ashirov <webapis@users.noreply.github.com>
2023-05-04 09:57:50 +0500

    dd

--- handlers/biraradamoda/ipekyol.js
+++ handlers/biraradamoda/ipekyol.js
@@ -1,69 +1,94 @@
 const Apify = require('apify');
 async function handler(page, context) {
 
-    const { request: { userData: { start } } } = context
+    const { request: { userData: {  } } } = context
     const url = await page.url()
 
     debugger;//
-    await page.waitForSelector('.prd-list .prd')
+    await page.waitForSelector('.prd-list')
     debugger;
+
+    await autoScroll(page)
     const data = await page.evaluate(() => {
-  
-        const items = Array.from(document.querySelectorAll('.prd-list .prd'))
+
+        const items = Array.from(document.querySelectorAll('.prd-list'))
         return items.map(item => {
 
             const priceNew = item.querySelector('.prd-list .prd-price .urunListe_satisFiyat') && item.querySelector('.prd-list .prd-price .urunListe_satisFiyat').textContent.replace('\n', '').replace('₺', '').trim()
 
             debugger;
-       
-            const longlink =item.querySelector('a.prd-lnk.clicked-item')&& item.querySelector('a.prd-lnk.clicked-item').href
-            const link =longlink&& longlink.substring(longlink.indexOf('https://www.ipekyol.com.tr/')+27)
-            const longImgUrl =  item.querySelector('[data-image-src]') && item.querySelector('[data-image-src]').getAttribute('data-image-src')
-            const imageUrlshort = longImgUrl&& longImgUrl.substring(longImgUrl.indexOf('https://img2-ipekyol.mncdn.com/mnresize/')+40)
+
+            const longlink = item.querySelector('a.prd-lnk.clicked-item') && item.querySelector('a.prd-lnk.clicked-item').href
+            const link = longlink && longlink.substring(longlink.indexOf('https://www.ipekyol.com.tr/') + 27)
+            const longImgUrl = item.querySelector('[data-image-src]') && item.querySelector('[data-image-src]').getAttribute('data-image-src')
+            const imageUrlshort = longImgUrl && longImgUrl.substring(longImgUrl.indexOf('https://img2-ipekyol.mncdn.com/mnresize/') + 40)
             return {
-                title: 'ipekyol '+ item.querySelector('.prd-name').innerText.replace(/İ/g,'i').toLowerCase(),
-        
+                title: item.querySelector('.prd-name span') && 'ipekyol ' + item.querySelector('.prd-name span').innerHTML.replace(/İ/g, 'i').toLowerCase(),
+
                 priceNew,//:priceNew.replace('.','').replace(',00','').trim(),
-  
-                imageUrl:imageUrlshort,
+
+                imageUrl: imageUrlshort,
                 link,
-     
+
                 timestamp: Date.now(),
-      
+
                 marka: 'ipekyol',
 
 
 
             }
         }).filter(f => f.imageUrl !== null)
-    } )
+    })
 
     console.log('data length_____', data.length)
-    const nextPageExists = await page.$('.btnDefault.load-next')
-    debugger;
-    if (nextPageExists && start) {
+    // const nextPageExists = await page.$('.btnDefault.load-next')
+    // debugger;
+    // if (nextPageExists && start) {
 
 
-        const nextPage = `${url}?page=2`
-        const requestQueue = await Apify.openRequestQueue();
-        debugger;
-        requestQueue.addRequest({ url: nextPage, userData: { start: false } })
-    } else if (nextPageExists && !start) {
-        debugger;
-        const pageUrl = url.slice(0, url.lastIndexOf("=") + 1)
-        const pageNumber = parseInt(url.substr(url.indexOf("=") + 1)) + 1
-        const nextPage = pageUrl + pageNumber
-        const requestQueue = await Apify.openRequestQueue();
-        debugger;
-        requestQueue.addRequest({ url: nextPage, userData: { start: false} })
+    //     const nextPage = `${url}?page=2`
+    //     const requestQueue = await Apify.openRequestQueue();
+    //     debugger;
+    //     requestQueue.addRequest({ url: nextPage, userData: { start: false } })
+    // } else if (nextPageExists && !start) {
+    //     debugger;
+    //     const pageUrl = url.slice(0, url.lastIndexOf("=") + 1)
+    //     const pageNumber = parseInt(url.substr(url.indexOf("=") + 1)) + 1
+    //     const nextPage = pageUrl + pageNumber
+    //     const requestQueue = await Apify.openRequestQueue();
+    //     debugger;
+    //     requestQueue.addRequest({ url: nextPage, userData: { start: false } })
 
-    }
+    // }
 
     console.log('data length_____', data.length, 'url:', url)
 
-    return data.map(m=>{return {...m,title:m.title+" _"+process.env.GENDER }})
+    return data.map(m => { return { ...m, title: m.title + " _" + process.env.GENDER } })
 }
 
+async function autoScroll(page) {
+    await page.evaluate(async () => {
+
+
+        await new Promise((resolve, reject) => {
+            var totalHeight = 0;
+            var distance = 100;
+            let inc = 0
+            var timer = setInterval(() => {
+          
+                const totalItems = parseInt(document.querySelector('.prd-qty').innerHTML.replace(/[^\d]/g, ''))
+                const callectedItems = document.querySelectorAll('.prd').length
+                window.scrollBy(0, distance);
+                totalHeight += distance;
+                inc = inc + 1
+                if (callectedItems>=totalItems  ) {
+                    clearInterval(timer);
+                    resolve();
+                }
+            }, 200);
+        });
+    });
+}
 async function getUrls(page, param) {
 
     return { pageUrls: [], productCount: 0, pageLength: 0 }
